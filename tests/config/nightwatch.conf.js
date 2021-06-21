fe44d02b4176154e953ea4f61aaee58ececa5540
Jia Ming <62232054+anakin001@users.noreply.github.com>
2021-06-21 15:46:12 +0800

    Update nightwatch.conf.js

--- tests/config/nightwatch.conf.js
+++ tests/config/nightwatch.conf.js
@@ -5,7 +5,7 @@ const Services = {}; loadServices();
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: [],
+  src_folders: ["./tests"],
 
   // See https://nightwatchjs.org/guide/working-with-page-objects/
   page_objects_path: ["tests/page_object"],
