ad3e6207123ac9a5de97377df85cc63ecc0fe7b7
shinneng <shinneng@users.noreply.github.com>
2021-06-29 09:17:44 +0000

    deploy: 8de92118dbba287c7757d7012e67119ce0631aa0

---
+++ assets/js/48.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[48],{
+
+/***/ "./src/modules/services/submodule/voting/img/badge-silver-proximax-sirius-wallet.svg":
+/*!*******************************************************************************************!*\
+  !*** ./src/modules/services/submodule/voting/img/badge-silver-proximax-sirius-wallet.svg ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/badge-silver-proximax-sirius-wallet.3291fc4b.svg\";\n\n//# sourceURL=webpack:///./src/modules/services/submodule/voting/img/badge-silver-proximax-sirius-wallet.svg?");
+
+/***/ })
+
+}]);
