289cfc5cfd362fbcaf2716fe65a2f3254f438e57
laics1984 <laics1984@users.noreply.github.com>
2021-06-30 03:29:54 +0000

    deploy: e681be16256345d34ed5452332e87a7cc9a41815

--- assets/js/48.js
+++ assets/js/48.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[48],{
 
-/***/ "./src/modules/services/submodule/voting/img/badge-silver-proximax-sirius-wallet.svg":
-/*!*******************************************************************************************!*\
-  !*** ./src/modules/services/submodule/voting/img/badge-silver-proximax-sirius-wallet.svg ***!
-  \*******************************************************************************************/
+/***/ "./src/modules/services/img/icon-account-green-16h-proximax-sirius-wallet.svg":
+/*!************************************************************************************!*\
+  !*** ./src/modules/services/img/icon-account-green-16h-proximax-sirius-wallet.svg ***!
+  \************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/badge-silver-proximax-sirius-wallet.3291fc4b.svg\";\n\n//# sourceURL=webpack:///./src/modules/services/submodule/voting/img/badge-silver-proximax-sirius-wallet.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-account-green-16h-proximax-sirius-wallet.f8fb8a8a.svg\";\n\n//# sourceURL=webpack:///./src/modules/services/img/icon-account-green-16h-proximax-sirius-wallet.svg?");
 
 /***/ })
 
