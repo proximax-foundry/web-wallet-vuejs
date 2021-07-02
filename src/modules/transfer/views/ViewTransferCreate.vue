1cf4bd08285e397b1ef8af03b5f050ba7e2645ba
jiaminglim <jiaminglim0828@gmail.com>
2021-07-02 19:34:24 +0800

    reformatted

--- src/modules/transfer/views/ViewTransferCreate.vue
+++ src/modules/transfer/views/ViewTransferCreate.vue
@@ -14,62 +14,25 @@
         <div class="mb-5 border-b border-gray-200">
           <div class="error error_box" v-if="err != ''">{{ err }}</div>
           <div v-if="moreThanOneAccount" class="text-left p-4">
-            <div
-              class="mb-1 cursor-pointer z-20 border-b border-gray-200"
-              @click="showMenu = !showMenu"
-            >
+            <div class="mb-1 cursor-pointer z-20 border-b border-gray-200" @click="showMenu = !showMenu">
               <div class="font-bold text-xs">
                 {{ selectedAccName }}
-                <span
-                  v-if="isMultiSigBool"
-                  class="
-                    text-xs
-                    font-normal
-                    ml-2
-                    inline-block
-                    py-1
-                    px-2
-                    rounded
-                    bg-blue-200
-                    text-gray-800
-                  "
-                  >Multisig</span
-                >
+                <span v-if="isMultiSigBool" class="text-xs font-normal ml-2 inline-block py-1 px-2 rounded bg-blue-200 text-gray-800">
+                  Multisig
+                </span>
               </div>
               <div class="text-gray-400 mt-1 text-sm">{{ selectedAccAdd }}</div>
             </div>
             <transition name="slide">
               <div v-if="showMenu" class="z-10">
-                <div
-                  :key="item.address"
-                  :i="index"
-                  v-for="(item, index) in accounts"
-                  class="p-2 cursor-pointer"
-                  :class="
-                    item.name == selectedAccName
-                      ? 'bg-blue-primary text-white font-bold'
-                      : 'text-gray-800 bg-gray-50 optionDiv'
-                  "
-                  @click="changeSelection(item)"
-                  :title="'Address is ' + item.address"
-                >
+                <div :key="item.address" :i="index" v-for="(item, index) in accounts" class="p-2 cursor-pointer" :class=" item.name == selectedAccName ? 'bg-blue-primary text-white font-bold': 'text-gray-800 bg-gray-50 optionDiv'"
+                  @click="changeSelection(item)" :title="'Address is ' + item.address" >
                   <div>
                     {{ item.name }}
                     <span
-                      v-if="isMultiSig(item.address)"
-                      class="
-                        text-xs
-                        font-normal
-                        ml-2
-                        inline-block
-                        py-1
-                        px-2
-                        rounded
-                        bg-blue-200
-                        text-gray-800
-                      "
-                      >Multisig</span
-                    >
+                      v-if="isMultiSig(item.address)" class="text-xs font-normal ml-2 inline-block py-1 px-2 rounded bg-blue-200 text-gray-800">
+                        Multisig
+                    </span>
                   </div>
                 </div>
               </div>
@@ -80,76 +43,37 @@
             <div v-if="getWalletCosigner().list.length > 0">
               <div class="text-tsm">
                 Cosigner:
-                <span
-                  class="font-bold"
-                  v-if="getWalletCosigner().list.length == 1"
-                  >{{ getWalletCosigner().list[0].name }} (Balance:
-                  {{ getWalletCosigner().list[0].balance }} XPX)
-                  <span
-                    v-if="
-                      getWalletCosigner().list[0].balance <
-                      lockFundTotalFee.value
-                    "
-                    class="error"
-                    >- Insufficient balance</span
-                  ></span
-                >
-                <span class="font-bold" v-else
-                  ><select v-model="cosignAddress">
-                    <option
-                      v-for="(element, item) in getWalletCosigner().list"
-                      :value="element.address"
-                      :key="item"
-                    >
+                <span class="font-bold" v-if="getWalletCosigner().list.length == 1">
+                  {{ getWalletCosigner().list[0].name }} (Balance:{{ getWalletCosigner().list[0].balance }} XPX)
+                    <span v-if="getWalletCosigner().list[0].balance <lockFundTotalFee.value" class="error">
+                      - Insufficient balance
+                    </span>
+                </span>
+                <span class="font-bold" v-else>
+                  <select v-model="cosignAddress">
+                    <option v-for="(element, item) in getWalletCosigner().list" :value="element.address" :key="item">
                       {{ element.name }} (Balance: {{ element.balance }} XPX)
                     </option>
-                  </select></span
-                >
+                  </select>
+                </span>
                 <div v-if="cosignerBalanceInsufficient" class="error">
                   - Insufficient balance
                 </div>
               </div>
             </div>
-            <div class="error" v-else>No eligible cosigner in this wallet</div>
+            <div class="error" v-else>
+              No eligible cosigner in this wallet
+            </div>
           </div>
-          <SelectInputPlugin
-            v-if="showContactSelection"
-            placeholder="Contact"
-            errorMessage=""
-            v-model="selectContact"
-            :options="contact"
-            @default-selected="selectContact = 0"
-            @show-selection="updateAdd"
-          />
+          <SelectInputPlugin v-if="showContactSelection" placeholder="Contact" errorMessage=""  v-model="selectContact" :options="contact" @default-selected="selectContact = 0"  @show-selection="updateAdd"/>
           <div class="flex">
             <div class="flex-grow mr-5">
-              <TextInput
-                placeholder="Recipient"
-                :errorMessage="addressErrorMsg"
-                :showError="showAddressError"
-                v-model="recipient"
-                icon="wallet"
-                :disabled="disableRecipient"
-              />
+              <TextInput placeholder="Recipient" :errorMessage="addressErrorMsg" :showError="showAddressError" v-model="recipient" icon="wallet" :disabled="disableRecipient"/>
             </div>
             <div class="flex-none">
-              <div
-                class="
-                  rounded-full
-                  bg-gray-300
-                  w-14
-                  h-14
-                  cursor-pointer
-                  relative
-                "
-                style="top: -5px"
-                @click="showContactSelection = !showContactSelection"
-              >
-                <font-awesome-icon
-                  icon="id-card-alt"
-                  class="h-20 w-20 inline text-blue-primary absolute"
-                  style="top: -12px; left: 19px"
-                ></font-awesome-icon>
+              <div class="rounded-full bg-gray-300 w-14 h-14 cursor-pointer relative" style="top: -5px" @click="showContactSelection = !showContactSelection">
+                <font-awesome-icon icon="id-card-alt" class="h-20 w-20 inline text-blue-primary absolute" style="top: -12px; left: 19px"> 
+                </font-awesome-icon>
               </div>
             </div>
           </div>
@@ -157,190 +81,71 @@
         <div class="text-left p-3 pb-0 border-l-8 border-gray-100">
           <div class="bg-gray-100 rounded-2xl p-3">
             <div class="inline-block mr-4 tfaddext-tsm">
-              <img
-                src="@/assets/img/icon-prx-xpx-blue.svg"
-                class="w-5 inline mr-1"
-              />Balance: <span class="text-xs">{{ balance }} XPX</span>
+              <img src="@/assets/img/icon-prx-xpx-blue.svg" class="w-5 inline mr-1"/>
+                Balance: 
+              <span class="text-xs">
+                {{ balance }} XPX
+              </span>
             </div>
           </div>
-          <SupplyInput
-            v-model="sendXPX"
-            title="Send"
-            :balance="Number(balance)"
-            placeholder="Enter Amount"
-            type="text"
-            icon="coins"
-            :showError="showBalanceErr"
-            errorMessage="Insufficient balance"
-            :decimal="6"
-            class="mt-5"
-            :disabled="disableSupply"
-          />
+          <SupplyInput v-model="sendXPX" title="Send" :balance="Number(balance)" placeholder="Enter Amount" type="text" icon="coins" :showError="showBalanceErr" errorMessage="Insufficient balance" :decimal="6" class="mt-5" :disabled="disableSupply"/>
         </div>
         <div v-for="(mosaic, index) in mosaicsCreated" :key="index">
-          <MosaicInput
-            placeholder="Select mosaic"
-            errorMessage=""
-            v-model="selectedMosaic[index].id"
-            :index="index"
-            :options="mosaics"
-            :disableOptions="selectedMosaic"
-            @show-mosaic-selection="updateMosaic"
-            @remove-mosaic-selected="removeMosaic"
-          />
-          <SupplyInput
-            v-if="selectedMosaic[index].id != 0"
-            v-model="selectedMosaic[index].amount"
-            :balance="getSelectedMosaicBalance[index]"
-            placeholder="Enter Amount"
-            type="text"
-            icon="coins"
-            :showError="showBalanceErr"
-            errorMessage="Insufficient balance"
-            :decimal="mosaicSupplyDivisibility[index]"
-          />
+          <MosaicInput placeholder="Select mosaic" errorMessage="" v-model="selectedMosaic[index].id" :index="index" :options="mosaics" :disableOptions="selectedMosaic" @show-mosaic-selection="updateMosaic" @remove-mosaic-selected="removeMosaic"/>
+          <SupplyInput v-if="selectedMosaic[index].id != 0" v-model="selectedMosaic[index].amount" :balance="getSelectedMosaicBalance[index]" placeholder="Enter Amount" type="text" icon="coins" :showError="showBalanceErr" errorMessage="Insufficient balance" :decimal="mosaicSupplyDivisibility[index]"/>
         </div>
         <div>
-          <button
-            class="
-              mb-5
-              hover:shadow-lg
-              bg-white
-              hover:bg-gray-100
-              rounded-3xl
-              border-2
-              font-bold
-              px-6
-              py-2
-              border-blue-primary
-              text-blue-primary
-              outline-none
-              focus:outline-none
-              disabled:opacity-50
-            "
-            :disabled="addMosaicsButton"
-            @click="displayMosaicsOption"
-          >
+          <button class="mb-5 hover:shadow-lg bg-white hover:bg-gray-100 rounded-3xl border-2 font-bold px-6 py-2 border-blue-primary text-blue-primary outline-none focus:outline-none disabled:opacity-50" :disabled="addMosaicsButton" @click="displayMosaicsOption">
             (+) Add Mosaics
           </button>
         </div>
         <div class="mb-5 border-t pt-4 border-gray-200">
           <div class="rounded-2xl bg-gray-100 p-5">
-            <input
-              id="regularMsg"
-              type="radio"
-              name="msgOption"
-              value="regular"
-              v-model="msgOption"
-              @change="clearMsg()"
-              :disabled="disableRegularMsg == 1"
-            /><label for="regularMsg" class="cursor-pointer font-bold ml-4 mr-5"
-              >Regular</label
-            >
-            <input
-              id="hexMsg"
-              type="radio"
-              name="msgOption"
-              value="hex"
-              v-model="msgOption"
-              @change="clearMsg()"
-              :disabled="disableHexMsg == 1"
-            /><label for="hexMsg" class="cursor-pointer font-bold ml-4"
-              >Hexadecimal</label
-            >
+            <input id="regularMsg" type="radio" name="msgOption" value="regular" v-model="msgOption" @change="clearMsg()" :disabled="disableRegularMsg == 1"/>
+            <label for="regularMsg" class="cursor-pointer font-bold ml-4 mr-5"> 
+              Regular
+            </label>
+            <input id="hexMsg" type="radio" name="msgOption" value="hex" v-model="msgOption" @change="clearMsg()" :disabled="disableHexMsg == 1"/>
+            <label for="hexMsg" class="cursor-pointer font-bold ml-4"
+              >Hexadecimal
+              </label>
           </div>
         </div>
         <div class="mb-5" v-if="!encryptedMsgDisable">
           <div class="rounded-2xl bg-gray-100 p-5">
-            <input
-              id="encryptedMsg"
-              type="checkbox"
-              value="encryptedMsg"
-              v-model="encryptedMsg"
-              :disabled="disableEncryptMsg == 1"
-            /><label
-              for="encryptedMsg"
-              class="cursor-pointer font-bold ml-4 mr-5 text-tsm"
-              >Encrypted</label
-            >
+            <input id="encryptedMsg"  type="checkbox" value="encryptedMsg" v-model="encryptedMsg" :disabled="disableEncryptMsg == 1"/>
+            <label for="encryptedMsg" class="cursor-pointer font-bold ml-4 mr-5 text-tsm">
+              Encrypted
+            </label>
           </div>
         </div>
-        <TransferTextareaInput
-          placeholder="Message"
-          errorMessage=""
-          v-model="messageText"
-          :remainingChar="remainingChar"
-          :limit="messageLimit"
-          icon="comment"
-          class="mt-5"
-          :msgOpt="msgOption"
-          :disabled="disableMsgInput"
-        />
+        <TransferTextareaInput placeholder="Message" errorMessage="" v-model="messageText" :remainingChar="remainingChar" :limit="messageLimit" icon="comment" class="mt-5" :msgOpt="msgOption" :disabled="disableMsgInput"/>
         <div class="rounded-2xl bg-gray-100 p-5">
           <div class="inline-block mr-4 text-xs">
-            <img
-              src="@/assets/img/icon-prx-xpx-blue.svg"
-              class="w-5 inline mr-1 text-gray-500"
-            />Unconfirmed/Recommended Fee: {{ effectiveFee }} XPX
+            <img src="@/assets/img/icon-prx-xpx-blue.svg" class="w-5 inline mr-1 text-gray-500"/>
+            Unconfirmed/Recommended Fee: {{ effectiveFee }} XPX
           </div>
         </div>
-        <div
-          class="
-            p-4
-            rounded-xl
-            bg-gray-100
-            mt-2
-            items-center
-            w-full
-            text-xs text-gray-800
-          "
-          v-if="isMultiSig(selectedAccAdd)"
-        >
+        <div class="p-4 rounded-xl bg-gray-100 mt-2 items-center w-full text-xs text-gray-800 " v-if="isMultiSig(selectedAccAdd)">
           <div class="text-center">
             <div class="inline-block">
               <div class="flex">
-                <img
-                  src="@/assets/img/icon-prx-xpx-blue.svg"
-                  class="w-5 inline-block mr-1 self-center"
-                />
+                <img src="@/assets/img/icon-prx-xpx-blue.svg" class="w-5 inline-block mr-1 self-center"/>
                 <div class="inline-block self-center text-left">
                   <div>LockFund: {{ lockFundCurrency }} {{ currencyName }}</div>
-                  <div>
-                    Unconfirmed/Recommended Fee: {{ lockFundTxFee }}
-                    {{ currencyName }}
+                  <div> Unconfirmed/Recommended Fee: {{ lockFundTxFee }}{{ currencyName }}
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <PasswordInput
-          placeholder="Enter Wallet Password"
-          :errorMessage="
-            'Please enter wallet ' +
-           /* walletState.currentLoggedInWallet.name + */
-            '\'s password'
-          "
-          :showError="showPasswdError"
-          v-model="walletPassword"
-          icon="lock"
-          class="mt-5"
-          :disabled="disablePassword"
-        />
+        <PasswordInput placeholder="Enter Wallet Password" :errorMessage="'Please enter wallet ' + /* walletState.currentLoggedInWallet.name + */'\'s password'" :showError="showPasswdError" v-model="walletPassword" icon="lock" class="mt-5" :disabled="disablePassword"/>
         <div class="mt-10">
-          <button
-            type="button"
-            class="default-btn mr-5 focus:outline-none"
-            @click="clearInput()"
-          >
+          <button type="button" class="default-btn mr-5 focus:outline-none" @click="clearInput()">
             Clear
           </button>
-          <button
-            type="submit"
-            class="default-btn py-1 disabled:opacity-50"
-            :disabled="disableCreate"
-            @click="makeTransfer()"
-          >
+          <button type="submit" class="default-btn py-1 disabled:opacity-50" :disabled="disableCreate" @click="makeTransfer()">
             Create
           </button>
         </div>
@@ -351,7 +156,7 @@
   </div>
 </template>
 <script >
-import { Helper } from '@/util/typeHelper';
+import { Helper } from "@/util/typeHelper";
 import { computed, ref, getCurrentInstance, watch } from "vue";
 import TextInput from "@/components/TextInput.vue";
 import PasswordInput from "@/components/PasswordInput.vue";
@@ -372,7 +177,7 @@ import { multiSign } from "@/util/multiSignatory";
 import { walletState } from "@/state/walletState";
 import { networkState } from "@/state/networkState";
 import { accountUtils } from "@/util/accountUtils";
-import {TransactionUtils} from '@/util/transactionUtils'
+import { TransactionUtils } from "@/util/transactionUtils";
 import { WalletUtils } from "@/util/walletUtils";
 
 export default { 
@@ -408,7 +213,7 @@ export default {
     const selectedMosaicAmount = ref([]);
     const mosaicSupplyDivisibility = ref([]);
     const currentlySelectedMosaic = ref([]);
-    const sendXPX = ref('0');
+    const sendXPX = ref("0");
     const encryptedMsgDisable = ref(true);
     const toggleConfirm = ref(false);
     const forceSend = ref(false);
@@ -514,13 +319,17 @@ export default {
     /* appStore.getFirstAccAdd() */
     const balance = computed(() => {
       if (walletState.currentLoggedInWallet) {
-        return walletState.currentLoggedInWallet.accounts.find(element => element.address === selectedAccAdd.value).balance;
+        return walletState.currentLoggedInWallet.accounts.find(
+          (element) => element.address === selectedAccAdd.value
+        ).balance;
       } else {
         return "0";
       }
     });
     const isMultiSigBool = ref(
-      isMultiSig(walletState.currentLoggedInWallet.selectDefaultAccount().address)
+      isMultiSig(
+        walletState.currentLoggedInWallet.selectDefaultAccount().address
+      )
     );
     /*  appStore.getFirstAccAdd() */
 
@@ -560,9 +369,7 @@ export default {
       selectedAccName.value = i.name;
       selectedAccAdd.value = i.address;
       // balance.value = i.balance;
-      balance.value == 0
-        ? (showBalanceErr.value = true)
-        : (showBalanceErr.value = false);
+      balance.value == 0? (showBalanceErr.value = true): (showBalanceErr.value = false);
       showMenu.value = !showMenu.value;
       currentSelectedName.value = i.name;
       isMultiSigBool.value = isMultiSig(i.address);
@@ -717,7 +524,7 @@ export default {
       if (mosaic != undefined) {
         return mosaic.amount;
       } else {
-        return '0';
+        return "0";
       }
     };
 
@@ -764,7 +571,6 @@ export default {
             text: i.idHex + " > Balance: " + Helper.amountFormatterSimple(i.amount,i.divisibility),
             id: index + 1,
           });
-          
         });
       }
       return mosaicOption;
