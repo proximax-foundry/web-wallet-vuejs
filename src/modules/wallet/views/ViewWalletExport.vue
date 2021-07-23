d24abb3a9e021a64c2494f93206d86e2c40f7a4f
Benjamin - workstation <benjamin@webtree.my>
2021-07-23 22:03:36 +0800

    fix mobile view on vue pages

--- src/modules/wallet/views/ViewWalletExport.vue
+++ src/modules/wallet/views/ViewWalletExport.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="flex justify-between text-sm">
+<div class="flex justify-between text-xs sm:text-sm">
   <div><span class="text-gray-400">Wallet ></span> <span class="text-blue-primary font-bold">{{$t('services.exportwallet')}}</span></div>
   <div>
     <router-link :to="{ name: 'ViewWallets'}" class="font-bold" active-class="accounts">{{$t('services.deletewallet')}}</router-link>
