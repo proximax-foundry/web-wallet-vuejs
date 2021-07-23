b17301baa63e3ddf29759f48e83588c4db8c9888
Benjamin Lai <laics1984@gmail.com>
2021-07-23 16:48:02 +0800

    fix template styling on mobile (#153)
    Co-authored-by: Benjamin - workstation <benjamin@webtree.my>

--- src/modules/home/views/ViewHome.vue
+++ src/modules/home/views/ViewHome.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="container mx-auto text-center md:px-20 lg:px-40 mt-10">
     <h1 class="big-title">{{$t('Header.siriuswallet')}}</h1>
-    <h2>{{$t('welcome.titledescription')}}.</h2>
-    <div class="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-10">
+    <h2 class="big-para">{{$t('welcome.titledescription')}}.</h2>
+    <div class="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-2 sm:mt-10">
       <div class="text-center p-5">
-        <img src="@/modules/home/img/icon-blockchain-full-color-80h-proximax-sirius-wallet.svg" class="w-36 md:w-28 lg:w-18 inline-block mb-3">
-        <div class="font-bold text-md md:text-md lg:text-lg">{{$t('welcome.blockchain')}}</div>
-        <div class=" sm:text-tsm md:text-tmd lg:text-tlg">{{$t('welcome.blockchaindescription')}}.</div>
+        <img src="@/modules/home/img/icon-blockchain-full-color-80h-proximax-sirius-wallet.svg" class="w-20 md:w-28 lg:w-18 inline-block mb-3">
+        <div class="font-bold text-sm md:text-md lg:text-lg">{{$t('welcome.blockchain')}}</div>
+        <div class="text-xs sm:text-tsm md:text-tmd lg:text-tlg">{{$t('welcome.blockchaindescription')}}.</div>
       </div>
       <div class="text-center p-5">
-        <img src="@/modules/home/img/icon-storage-full-color-80h-proximax-sirius-wallet.svg" class="w-36 md:w-28 lg:w-18 inline-block mb-3">
-        <div class="font-bold text-md md:text-md lg:text-lg">{{$t('welcome.storage')}}</div>
-        <div class=" sm:text-tsm md:text-tmd lg:text-tlg">{{$t('welcome.storagedescription')}}.</div>
+        <img src="@/modules/home/img/icon-storage-full-color-80h-proximax-sirius-wallet.svg" class="w-20 md:w-28 lg:w-18 inline-block mb-3">
+        <div class="font-bold text-sm md:text-md lg:text-lg">{{$t('welcome.storage')}}</div>
+        <div class="text-xs sm:text-tsm md:text-tmd lg:text-tlg">{{$t('welcome.storagedescription')}}.</div>
       </div>
       <div class="text-center p-5">
-        <img src="@/modules/home/img/icon-streaming-full-color-80h-proximax-sirius-wallet.svg" class="w-36 md:w-28 lg:w-18 inline-block mb-3">
-        <div class="font-bold text-md md:text-md lg:text-lg">{{$t('welcome.streaming')}}</div>
-        <div class=" sm:text-tsm md:text-tmd lg:text-tlg">{{$t('welcome.streamingdescription')}}.</div>
+        <img src="@/modules/home/img/icon-streaming-full-color-80h-proximax-sirius-wallet.svg" class="w-20 md:w-28 lg:w-18 inline-block mb-3">
+        <div class="font-bold text-sm md:text-md lg:text-lg">{{$t('welcome.streaming')}}</div>
+        <div class="text-xs sm:text-tsm md:text-tmd lg:text-tlg">{{$t('welcome.streamingdescription')}}.</div>
       </div>
       <div class="text-center p-5">
-        <img src="@/modules/home/img/icon-supercontracts-full-color-80h-proximax-sirius-wallet.svg" class="w-36 md:w-28 lg:w-18 inline-block mb-3">
-        <div class="font-bold text-md md:text-md lg:text-lg">{{$t('welcome.supercontracts')}}</div>
-        <div class=" sm:text-tsm md:text-tmd lg:text-tlg">{{$t('welcome.supercontractdescription')}}.</div>
+        <img src="@/modules/home/img/icon-supercontracts-full-color-80h-proximax-sirius-wallet.svg" class="w-20 md:w-28 lg:w-18 inline-block mb-3">
+        <div class="font-bold text-sm md:text-md lg:text-lg">{{$t('welcome.supercontracts')}}</div>
+        <div class="text-xs sm:text-tsm md:text-tmd lg:text-tlg">{{$t('welcome.supercontractdescription')}}.</div>
       </div>
     </div>
     <div class="inline-block mt-5">
-      <div class="gray-line py-16 grid xs:grid-cols-1 md:grid-cols-3">
+      <div class="gray-line py-5 sm:py-16 grid xs:grid-cols-1 md:grid-cols-3">
         <div class="px-5 self-center">
           <SignInModal />
         </div>
