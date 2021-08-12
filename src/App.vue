f6d6358bcbaf031916fab2efe5a8793a3db9ce14
Chun Lam <chunlam.sees@gmail.com>
2021-08-12 20:42:05 +0800

    bump version 0.4.8

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
     <footer class="h-12 text-center mt-20">
 
       <div class="text-xs py-2">
-        <div class="font-bold">version 0.4.7</div>
+        <div class="font-bold">version 0.4.8</div>
         <div class="font-bold">&copy; ProximaX 2021</div>
         <div>Please report any issues identified to our <a href="https://t.me/proximaxhelpdesk" target=_new class="text-blue-primary font-bold hover:underline">helpdesk</a>.</div>
       </div>
