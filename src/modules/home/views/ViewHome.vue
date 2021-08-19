b44819dc8138209d9e7f04992945bbf44e936068
Benjamin - workstation <benjamin@webtree.my>
2021-08-19 11:55:16 +0800

    change label on home page

--- src/modules/home/views/ViewHome.vue
+++ src/modules/home/views/ViewHome.vue
@@ -5,7 +5,7 @@
       <div class="text-center p-5">
         <img src="@/modules/services/img/icon-swap-process-color-80h-proximax-sirius-wallet.svg" class="w-20 md:w-28 lg:w-18 inline-block mb-3">
         <div class="font-bold text-sm md:text-md lg:text-lg">Swap</div>
-        <div class="text-xs sm:text-tsm md:text-tmd lg:text-tlg">Swap from eXPX or bXPX to Sirius.</div>
+        <div class="text-xs sm:text-tsm md:text-tmd lg:text-tlg text-gray-500">Swap from Ethereum <span class="text-txs text-black font-bold">ETH</span> / Binance Smart Change <span class="text-txs text-black font-bold">BSC</span> to Sirius <span class="text-txs text-black font-bold">XPX</span>.</div>
       </div>
     </div>
     <div class="inline-block mt-5">
