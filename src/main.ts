e09e3c51d2ff6c8cb812cff522852529fc56cd31
jiaming <jiaminglim0828@gmail.com>
2021-09-02 14:40:33 +0800

    fixed import

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import ConfirmDialog from 'primevue/confirmdialog';
 import Toast from 'primevue/toast';
 import i18n from './i18n'
-
+import VWave from 'v-wave'
 library.add(fas, faTimes, faEye, faEyeSlash, faLock, faWallet, faKey, faCheck, faExclamation, faBars, faCopy, faSignOutAlt, faCaretDown, faEdit, faTimesCircle, faCheckCircle, faTrashAlt, faIdCardAlt, faDownload, faCoins, faComment, faBell, faCircle, faChevronUp, faChevronDown, faTrashRestore, faFileExport, faFileImport, faArrowRight, faAt, faExternalLinkAlt );
 const app = createApp(App);
 const emitter = mitt();
@@ -36,6 +36,7 @@ app.use(PrimeVue);
 app.use(ConfirmationService);
 app.use(ToastService);
 app.use(i18n);
+app.use(VWave);
 app.mount('#app');
 // Use Components
 app.component('ConfirmDialog', ConfirmDialog);
