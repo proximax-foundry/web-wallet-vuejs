b0296c7ffa3bcd3605d5f942ac9bd765b32c91c4
Benjamin - workstation <benjamin@webtree.my>
2021-09-24 19:22:44 +0800

    applied metamask provider for outgoing swap and fix bsc incoming swap check

--- src/modules/services/submodule/mainnetSwap/submodule2/eth/views/ViewServicesMainnetSwapCheckSiriusToETH.vue
+++ src/modules/services/submodule/mainnetSwap/submodule2/eth/views/ViewServicesMainnetSwapCheckSiriusToETH.vue
@@ -25,9 +25,28 @@
       <div class="text-lg my-7 font-bold">Check Swap Status</div>
       <div class="bg-yellow-200 text-yellow-900 text-tsm p-3 mb-5 rounded-2xl" v-if="!verifyMetaMaskPlugin">Please make sure there is no other crypto wallet extension currently being enabled except <b>MetaMask</b>.<div class="my-2">Refer to the <a href="https://bit.ly/3mVayCu" target=_new class="text-blue-primary">walkthrough<font-awesome-icon icon="external-link-alt" class="text-blue-primary w-3 h-3 self-center inline-block ml-1"></font-awesome-icon></a> for more details.</div>Please refresh this page after disabling other wallet extensions.</div>
       <div class="error error_box mb-5" v-if="serviceErr!=''">{{ serviceErr }}</div>
+      <div class="error error_box mb-5" v-if="err!=''">{{ err }}</div>
       <p class="font-bold text-tsm text-left mb-1">Type</p>
       <div class="mb-5 mt-3 text-left">
         <button class="border px-3 py-2 w-20 text-blue-primary font-bold rounded-l-xl cursor-pointer hover:border-blue-primary hover:bg-blue-50 transition-all duration-200" @click="$router.push({name: 'ViewServicesMainnetSwapCheckETHToSirius'})">In</button><button class="bg-blue-primary px-3 py-2 w-20 text-white font-bold rounded-r-xl border border-blue-primary cursor-auto">Out</button> <span class="text-gray-500 ml-3 text-tsm">From Sirius to ETH</span>
+      </div>
+      <p class="font-bold text-tsm text-left mb-1">MetaMask Address</p>
+      <div class="mb-5 flex justify-between bg-gray-100 rounded-2xl p-3 text-left" v-if="isInstallMetamask">
+        <div class="text-tsm text-gray-700 self-center relative">
+          <div><img src="@/modules/services/submodule/mainnetSwap/img/icon-metamask.svg" class="w-5 inline ml-1 mr-2 absolute" style="top: 0px;"> <div class="ml-8 inline-block break-all">{{ isMetamaskConnected?(currentAccount?currentAccount:'Not connected'):'Not connected' }}</div></div>
+        </div>
+        <div class="self-center">
+          <button @click="connectMetamask()" class="hover:shadow-lg bg-white hover:bg-gray-100 rounded-3xl border-2 font-bold px-6 py-1 border-blue-primary text-blue-primary outline-none focus:outline-none" v-if="!currentAccount">Connect to MetaMask</button>
+          <button class=" bg-green-50 rounded-3xl border font-bold px-6 py-1 border-green-500 text-green-500 text-tsm outline-none focus:outline-none cursor-auto" v-else>Connected</button>
+        </div>
+      </div>
+      <div class="mb-5 flex justify-between bg-yellow-200 rounded-2xl p-3 text-left" v-else>
+        <div class="text-tsm text-gray-700 self-center relative">
+          <div><img src="@/modules/services/submodule/mainnetSwap/img/icon-metamask.svg" class="w-5 inline ml-1 mr-2 absolute" style="top: 0px;"> <div class="ml-8 inline-block text-gray-800">MetaMask is not installed</div></div>
+        </div>
+        <div class="self-center">
+          <a href="https://metamask.io/" target=_new class="hover:shadow-lg bg-white hover:bg-gray-100 rounded-3xl border-2 font-bold px-6 py-2 border-blue-primary text-blue-primary outline-none focus:outline-none">Download MetaMask</a>
+        </div>
       </div>
       <p class="font-bold text-tsm text-left mb-1">Sirius Transaction Hash</p>
       <TextInput placeholder="Sirius Transaction Hash" errorMessage="Please key in valid transaction hash" :showError="showTxnHashError" v-model="siriusTxnHash" icon="hashtag" class="w-full" />
@@ -165,9 +184,97 @@ export default {
     const siriusTxnHash = ref('');
     const showTxnHashError = computed(()=> !siriusTxnHash.value.match(siriusTxnHashPattern) && siriusTxnHash.value.length > 0);
 
+    /* MetaMask integration */
+    let ethereumChainId = swapData.ETHChainId;
+    let ethereumNetworkName = swapData.ETHNetworkName;
+    const isInstallMetamask = ref(false);
+    const isMetamaskConnected = ref(false);
+    const currentAccount = ref(null);
+    const currentNetwork = ref('');
+
     const checkSwapStatusUrl = SwapUtils.getOutgoing_ETHCheckStatus_URL(swapData.swap_IN_SERVICE_URL);
     const ethScanUrl = swapData.ETHScanUrl;
     const remoteTxnLink = computed( () => ethScanUrl + remoteTxnHash.value);
+
+    let provider;
+    let signer;
+    if (typeof window.ethereum !== 'undefined') {
+      provider = new ethers.providers.Web3Provider(window.ethereum);
+      signer = provider.getSigner();
+      isInstallMetamask.value = true;
+      isMetamaskConnected.value = ethereum.isConnected()?true:false;
+      ethereum
+        .request({ method: 'eth_accounts' })
+        .then(fetchMetaAccount)
+        .catch((err) => {
+          console.error(err);
+        });
+      ethereum
+        .request({ method: 'eth_chainId' })
+        .then((metaChainId) => {
+          verifyChain(metaChainId);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      ethereum.on('accountsChanged', handleAccountsChanged);
+      ethereum.on('chainChanged', (metaChainId) => {
+        verifyChain(metaChainId);
+      });
+    }else{
+      console.log('MetaMask not installed')
+    }
+    function fetchMetaAccount(accounts) {
+      if (accounts.length === 0) {
+        // MetaMask is locked or the user has not connected any accounts
+        currentAccount.value = '';
+      } else if (accounts[0] !== currentAccount.value) {
+        currentAccount.value = accounts[0];
+      }
+      isMetamaskConnected.value = ethereum.isConnected()?true:false;
+    }
+    // For now, 'eth_accounts' will continue to always return an array
+    function handleAccountsChanged(accounts) {
+      if(window.ethereum.isMetaMask){
+        if (accounts.length === 0) {
+          // MetaMask is locked or the user has not connected any accounts
+          currentAccount.value = '';
+        } else if (accounts[0] !== currentAccount.value) {
+          currentAccount.value = accounts[0];
+          serviceErr.value = '';
+        }
+      }
+      isMetamaskConnected.value = ethereum.isConnected()?true:false;
+    }
+    function verifyChain(chainId){
+      currentNetwork.value = chainId;
+      if(ethereumChainId === parseInt(chainId)){
+        err.value = '';
+      }else{
+        err.value = 'Please select ' + ethereumNetworkName + ' on MetaMask to swap';
+      }
+    }
+    const connectMetamask = () => {
+      if(window.ethereum.isMetaMask == undefined){
+        verifyMetaMaskPlugin.value = false;
+      }else{
+        verifyMetaMaskPlugin.value = true;
+        ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then(fetchMetaAccount)
+        .catch((err) => {
+          if (err.code === 4001) {
+            // EIP-1193 userRejectedRequest error
+            // If this happens, the user rejected the connection request.
+            console.log('Please connect to MetaMask.');
+          } else {
+            if(err.code == '-32002'){
+              serviceErr.value = 'Please click on MetaMask extension to approve connection';
+            }
+          }
+        });
+      }
+    };
 
     const step1 = ref(false);
     const step2 = ref(false);
@@ -194,7 +301,7 @@ export default {
     const serviceErr = ref('');
     const isDisabledCheck = computed(() =>
       // verify it has been connected to MetaMask too
-      !(!(showTxnHashError.value && siriusTxnHash.value.length > 0) && siriusTxnHash.value.length > 0)
+      !(!err.value && !(showTxnHashError.value && siriusTxnHash.value.length > 0) && siriusTxnHash.value.length > 0)
     );
     const isInvalidSiriusTxnHash = ref(false);
 
@@ -301,6 +408,11 @@ export default {
     });
 
     return {
+      err,
+      isInstallMetamask,
+      connectMetamask,
+      isMetamaskConnected,
+      currentAccount,
       copy,
       currentPage,
       isDisabledValidate,
