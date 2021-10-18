e50142f6c36904a9cc5ebd456e1fc9b5526da57d
Benjamin Lai <laics1984@gmail.com>
2021-10-18 19:17:32 +0800

    Update sidebar - account links

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="flex flex-col">
     <div class="border-b border-gray-200 py-5 w-60 flex-grow-0">
-      <div class="my-3 px-10 font-txs text-gray-400">ACCOUNTS ({{ accounts.length }})</div>
+      <div class="my-3 px-10 font-txs text-gray-400">ACCOUNTS ({{ allAccounts.length }})</div>
       <div>
-        <div v-for="(item, index) in accounts" :key="index" class="link_block flex items-center"><div class="mr-2 bg-gray-200 rounded-full w-5 h-5 flex items-center"><div class="text-center w-full"><img src="@/assets/img/navi/icon-accounts-light.svg" class="h-3 w-3 inline-block relative"></div></div>{{ item.name }}</div>
+        <router-link :to="{ name: 'ViewAccountDetails', params: { address: item.address }}" v-for="(item, index) in accounts.splice(0,10)" :key="index" class="link_block flex items-center"><div class="mr-2 bg-gray-200 rounded-full w-5 h-5 flex items-center"><div class="text-center w-full"><img src="@/assets/img/navi/icon-accounts-light.svg" class="h-3 w-3 inline-block relative"></div></div>{{ item.name }}</router-link>
       </div>
-      <a href="#" class="block font-bold link_block"><img src="@/assets/img/navi/icon-add.svg" class="h-4 w-4 inline-block relative mr-1">Create New Account</a>
+      <router-link :to="{ name: 'ViewAccountDisplayAll'}" class="link_block flex items-center" v-if="allAccounts.length > 10"><img src="@/assets/img/navi/icon-accounts.svg" class="h-4 w-4 inline-block mr-1">View all accounts</router-link>
+      <router-link :to="{ name: 'ViewAccountCreateSelectType'}" class="block font-bold link_block"><img src="@/assets/img/navi/icon-add.svg" class="h-4 w-4 inline-block relative mr-1">Create New Account</router-link>
     </div>
     <div class="border-b border-gray-200 py-5 w-60 flex-grow-0">
       <div class="my-3 px-10 text-gray-400">CREATE</div>
@@ -15,13 +16,13 @@
     </div>
     <div class="border-b border-gray-200 py-5 w-60 flex-grow-0">
       <div class="my-3 px-10 text-gray-400">NAVIGATE</div>
-      <a href="#" class="link_block flex items-center"><img src="@/assets/img/navi/icon-wallets.svg" class="h-3 w-3 inline-block relative mr-2">Wallets</a>
+      <router-link :to="{ name : 'ViewWallets'}" class="link_block flex items-center"><img src="@/assets/img/navi/icon-wallets.svg" class="h-3 w-3 inline-block relative mr-2">Wallets</router-link>
       <router-link :to="{ name : 'ViewAccountDisplayAll'}" class="link_block flex items-center"><img src="@/assets/img/navi/icon-accounts.svg" class="h-3 w-3 inline-block relative mr-2">Accounts</router-link>
-      <a href="#" class="link_block flex items-center"><img src="@/assets/img/navi/icon-address-book.svg" class="h-3 w-3 inline-block relative mr-2">Address Book</a>
+      <router-link :to="{ name : 'ViewServicesAddressBook'}" class="link_block flex items-center"><img src="@/assets/img/navi/icon-address-book.svg" class="h-3 w-3 inline-block relative mr-2">Address Book</router-link>
     </div>
     <div class="flex-grow"></div>
     <div class="flex-glow-0">
-      <a href="#" class="signout_block flex items-center"><img src="@/assets/img/navi/icon-sign-out.svg" class="h-3 w-3 inline-block relative mr-2">Sign Out</a>
+      <a href="#" class="signout_block flex items-center"><img src="@/assets/img/navi/icon-sign-out.svg" class="h-3 w-3 inline-block relative mr-2" @click="logout()">{{$t('Header.signout')}}</a>
     </div>
 
     <!-- <font-awesome-icon @click="showhideMenu();" icon="bars" :class="menuColorClass" class="menuBar absolute w-4 cursor-pointer" style="right: 7px; top: 9px;" v-if="showBar"></font-awesome-icon> -->
@@ -49,7 +50,10 @@
 
 <script>
 import { computed } from "vue";
+import { useRouter } from "vue-router";
 import { walletState } from '@/state/walletState';
+import { WalletStateUtils } from "@/state/utils/walletStateUtils";
+import {useI18n} from 'vue-i18n';
 
 export default{
   name: 'NavigationMenu',
@@ -96,11 +100,13 @@ export default{
     window.removeEventListener("resize", this.navMenuHandler);
   },
   setup(){
-    const accounts = computed(
+    const {t} = useI18n();
+    const router = useRouter();
+    const allAccounts = computed(
       () => {
         if(walletState.currentLoggedInWallet){
           if(walletState.currentLoggedInWallet.others){
-            const concatOther = walletState.currentLoggedInWallet.accounts.concat(walletState.currentLoggedInWallet.others)
+            const concatOther = walletState.currentLoggedInWallet.accounts
             return concatOther;
           } else{
             return walletState.currentLoggedInWallet.accounts;
@@ -111,9 +117,27 @@ export default{
       }
     );
 
+    const accounts = computed(
+      () => {
+        if(walletState.currentLoggedInWallet){
+          return walletState.currentLoggedInWallet.accounts;
+        }else{
+          return null;
+        }
+      }
+    );
+
+    const logout = () => {
+      WalletStateUtils.doLogout();
+      router.push({ name: "Home"});
+      console.log('logout')
+    };
+
     return {
+      logout,
       walletState,
-      accounts
+      accounts,
+      allAccounts
     };
   }
 }
