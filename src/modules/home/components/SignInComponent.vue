8301b8798b7a3e0621ee392058ef24110333d9a6
jiaming <jiaminglim0828@gmail.com>
2021-11-22 23:23:02 +0800

    merge origin/develop

--- src/modules/home/components/SignInComponent.vue
+++ src/modules/home/components/SignInComponent.vue
@@ -3,19 +3,6 @@
     <form @submit.prevent="login">
       <fieldset>
         <div class="w-8/12 text-center mr-auto ml-auto error error_box" v-if="err!=''">{{ err }}</div>
-<<<<<<< HEAD
-            <SelectNetworkInput />
-            <div class='mt-2'></div>
-            <SelectWalletInput />
-            <div class='mt-2'></div>
-            <div class= 'text-center'>
-              <PasswordInput @space="space=true" @removeSpace='space=false' class = 'w-8/12 block ml-auto mr-auto' placeholder="Password" :errorMessage="$t('signin.passwordrequired')" :showError="showPasswdError" v-model="walletPassword" icon="lock" />
-            </div>
-          <div class=" text-center">
-            <button v-if='!space' type="submit" class="blue-btn bg-gray-primary  py-2.5 w-8/12 disabled:opacity-50" :disabled="disableSignin">{{$t('welcome.signin')}}</button>
-            <button v-else type="submit" class="blue-btn bg-gray-primary mt-4 py-2.5 w-8/12 disabled:opacity-50" :disabled="disableSignin">{{$t('welcome.signin')}}</button>
-          </div>
-=======
         <SelectNetworkInput />
         <div class='mt-2'></div>
         <SelectWalletInput />
@@ -27,7 +14,6 @@
           <button v-if='!space' type="submit" class="blue-btn bg-gray-primary  py-2.5 w-8/12 disabled:opacity-50" :disabled="disableSignin">{{$t('welcome.signin')}}</button>
           <button v-else type="submit" class="blue-btn bg-gray-primary mt-4 py-2.5 w-8/12 disabled:opacity-50" :disabled="disableSignin">{{$t('welcome.signin')}}</button>
         </div>
->>>>>>> origin/develop
       </fieldset>
     </form>
   </div>
