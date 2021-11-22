cf0e4fec0b2b3a567ed7fde82bc12b5991dcc7e5
laics1984 <laics1984@users.noreply.github.com>
2021-11-22 03:26:37 +0000

    deploy: 6ece605471ebd9e750ead66749a0b0599c1b2a43

---
+++ assets/js/69.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[69],{
+
+/***/ "./src/assets/img/icon-prx-xpx-blue.svg":
+/*!**********************************************!*\
+  !*** ./src/assets/img/icon-prx-xpx-blue.svg ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-prx-xpx-blue.026c6b2b.svg\";\n\n//# sourceURL=webpack:///./src/assets/img/icon-prx-xpx-blue.svg?");
+
+/***/ })
+
+}]);
