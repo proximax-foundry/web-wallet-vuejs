c710bf1bead0655034d901f65a39ed222f50e2de
laics1984 <laics1984@users.noreply.github.com>
2021-12-01 03:27:41 +0000

    deploy: 1e22a03367b23d6356bc1c652c72dab06e825963

--- assets/js/77.js
+++ assets/js/77.js
@@ -1,13 +1,35 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[77],{
 
-/***/ "./src/assets/img/icon-prx-xpx-blue.svg":
-/*!**********************************************!*\
-  !*** ./src/assets/img/icon-prx-xpx-blue.svg ***!
-  \**********************************************/
+/***/ "./src/assets/img/icon-plus.svg":
+/*!**************************************!*\
+  !*** ./src/assets/img/icon-plus.svg ***!
+  \**************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icon-prx-xpx-blue.026c6b2b.svg\";\n\n//# sourceURL=webpack:///./src/assets/img/icon-prx-xpx-blue.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-plus.1ff2f3fb.svg\";\n\n//# sourceURL=webpack:///./src/assets/img/icon-plus.svg?");
+
+/***/ }),
+
+/***/ "./src/modules/dashboard/img/icon-link-new.svg":
+/*!*****************************************************!*\
+  !*** ./src/modules/dashboard/img/icon-link-new.svg ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-link-new.d756c673.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-link-new.svg?");
+
+/***/ }),
+
+/***/ "./src/modules/dashboard/img/icon-more-options.svg":
+/*!*********************************************************!*\
+  !*** ./src/modules/dashboard/img/icon-more-options.svg ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-more-options.54384d4a.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-more-options.svg?");
 
 /***/ })
 
