d0c58672a7f5b38ff07243d51ff30d57d41a4c11
laics1984 <laics1984@users.noreply.github.com>
2021-12-13 01:47:34 +0000

    deploy: 8cd3fbae2593d620990429c3c3e5e61645334987

--- assets/js/81.js
+++ assets/js/81.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[81],{
 
-/***/ "./src/modules/account/img/proximax-logo.svg":
-/*!***************************************************!*\
-  !*** ./src/modules/account/img/proximax-logo.svg ***!
-  \***************************************************/
+/***/ "./src/modules/dashboard/img/arrow-transaction-sender-out-orange-proximax-sirius-explorer.svg":
+/*!****************************************************************************************************!*\
+  !*** ./src/modules/dashboard/img/arrow-transaction-sender-out-orange-proximax-sirius-explorer.svg ***!
+  \****************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/proximax-logo.cb656acf.svg\";\n\n//# sourceURL=webpack:///./src/modules/account/img/proximax-logo.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/arrow-transaction-sender-out-orange-proximax-sirius-explorer.541d4c2b.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/arrow-transaction-sender-out-orange-proximax-sirius-explorer.svg?");
 
 /***/ })
 
