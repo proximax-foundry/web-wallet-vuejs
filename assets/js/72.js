6748b5abc549073eb126568a28f60384155a2598
anakin001 <anakin001@users.noreply.github.com>
2021-12-22 16:59:58 +0000

    deploy: fdfdef6fae57a317cfa61cc7c7fc0cd5f2023af6

--- assets/js/72.js
+++ assets/js/72.js
@@ -12,25 +12,36 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var prim
 
 /***/ }),
 
-/***/ "./src/assets/img/icon-info.svg":
+/***/ "./src/assets/img/icon-plus.svg":
 /*!**************************************!*\
-  !*** ./src/assets/img/icon-info.svg ***!
+  !*** ./src/assets/img/icon-plus.svg ***!
   \**************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icon-info.c16cc71f.svg\";\n\n//# sourceURL=webpack:///./src/assets/img/icon-info.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-plus.1ff2f3fb.svg\";\n\n//# sourceURL=webpack:///./src/assets/img/icon-plus.svg?");
 
 /***/ }),
 
-/***/ "./src/modules/account/img/proximax-logo.svg":
-/*!***************************************************!*\
-  !*** ./src/modules/account/img/proximax-logo.svg ***!
-  \***************************************************/
+/***/ "./src/modules/dashboard/img/icon-link-new.svg":
+/*!*****************************************************!*\
+  !*** ./src/modules/dashboard/img/icon-link-new.svg ***!
+  \*****************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/proximax-logo.cb656acf.svg\";\n\n//# sourceURL=webpack:///./src/modules/account/img/proximax-logo.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-link-new.d756c673.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-link-new.svg?");
+
+/***/ }),
+
+/***/ "./src/modules/dashboard/img/icon-more-options.svg":
+/*!*********************************************************!*\
+  !*** ./src/modules/dashboard/img/icon-more-options.svg ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-more-options.54384d4a.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-more-options.svg?");
 
 /***/ }),
 
