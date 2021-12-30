deb7338a33bbce9c872306c26f9a4902011c692e
Shin Neng <48303181+shinneng@users.noreply.github.com>
2021-12-30 16:26:19 +0800

    Aggregate transfer txn kl (#328)
    * template for aggregate transactions services
    
    * Default template for aggregate transaction service  (#233)
    
    * template for aggregate transactions services
    
    Co-authored-by: Benjamin - workstation <benjamin@webtree.my>
    
    * added recipient const to vue
    
    * update01
    
    * Preexisting code fix
    
    * Add right-side border
    
    * first commit
    
    * alignment
    
    * password box realignment
    
    * 0 xpx pop up fix
    
    * save button bug fix
    
    * send aggregate button disabling bug fix
    
    * xpx processing fix
    
    * clear password input after sending

---
+++ src/modules/services/submodule/aggregateTransactions/routingAggregateTransactions.js
@@ -0,0 +1,11 @@
+export const AggregateTransactions = [
+  {
+    path: '/complete',
+    name: 'ViewServicesAggregateTransactionsComplete',
+    component: () => import('@/modules/services/submodule/aggregateTransactions/views/ViewServicesAggregateTransactionsComplete.vue'),
+    meta: {
+      title: "Complete",
+    }
+  },
+];
+
