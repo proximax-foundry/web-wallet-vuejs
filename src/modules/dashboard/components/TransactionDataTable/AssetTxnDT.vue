1905a618323ee72a676eaf2c1b45f2fdce495370
Benjamin - workstation <benjamin@webtree.my>
2022-01-10 11:33:58 +0800

    Optimized Asset datatable

--- src/modules/dashboard/components/TransactionDataTable/AssetTxnDT.vue
+++ src/modules/dashboard/components/TransactionDataTable/AssetTxnDT.vue
@@ -10,59 +10,109 @@
       paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
       currentPageReportTemplate=""
       >
-      <Column field="hash" header="TX HASH" headerStyle="width:100px">
+      <Column style="width: 200px" v-if="!wideScreen">
+        <template #body="{data}">
+          <div>
+            <div class="uppercase text-xxs text-gray-300 font-bold mb-1">Tx Hash</div>
+            <div class="uppercase font-bold text-txs"><span class="text-txs" v-tooltip.right="data.hash">{{data.hash.substring(0, 20) }}...</span></div>
+          </div>
+          <div>
+            <div class="uppercase text-xxs text-gray-300 font-bold mb-1 mt-5">Type</div>
+            <div class="flex items-center">
+              <div class="uppercase font-bold text-txs mr-2">{{data.type}}</div>
+            </div>
+          </div>
+          <div>
+            <div class="uppercase text-xxs text-gray-300 font-bold mb-1 mt-5">Asset ID</div>
+            <div class="flex items-center">
+              <div class="text-txs font-bold">{{ data.assetId }}{{ data.namespaceName ? `(${data.namespaceName})`: "" }}</div>
+            </div>
+          </div>
+        </template>
+      </Column>
+      <Column headerStyle="width:200px" v-if="!wideScreen">
+        <template #body="{data}">
+          <div v-if="selectedGroupType === transactionGroupType.CONFIRMED">
+            <div class="uppercase text-xxs text-gray-300 font-bold mb-1">Timestamp</div>
+            <div class="uppercase font-bold text-txs">{{ convertLocalTime(data.timestamp) }}</div>
+          </div>
+          <div>
+            <div class="uppercase text-xxs text-gray-300 font-bold mb-1 mt-5">Info</div>
+            <div>
+              <div class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mr-1 rounded" v-if="data.nonce">{{ `Nonce: ${data.nonce}` }}</div>
+              <div class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mr-1 rounded" v-if="data.divisibility !== null">{{ `Divisibility: ${data.divisibility}` }}</div>
+              <div v-bind:class="['inline-block','text-black', 'py-1', 'px-2', 'my-1', 'mr-1', 'text-txs', 'font-bold', 'rounded', (data.transferable) ? 'bg-green-200 text-green-700' : 'bg-red-200 text-red-700']" v-if="data.transferable !== null">Transferable</div>
+              <div v-bind:class="['inline-block','text-black', 'py-1', 'px-2', 'my-1', 'mr-1', 'text-txs', 'font-bold', 'rounded', (data.supplyMutable) ? 'bg-green-200 text-green-700' : 'bg-red-200 text-red-700']" v-if="data.supplyMutable !== null">Supply Mutable</div>
+              <div class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mr-1 rounded" v-if="data.duration">{{ `Duration: ${data.duration}` }}</div>
+              <div class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mr-1 rounded" v-if="data.supplyDelta !== null">Supply:{{ data.supplyDelta > 0 ? "+" + data.supplyDelta : data.supplyDelta }}</div>
+              <div v-if="data.levyAssetId">
+                <div class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mr-1 rounded">
+                  Levy: {{ data.levyAssetId }}{{ data.levyAssetName ? `(${data.levyAssetName})`: "" }}
+                </div>
+                <div class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mr-1 rounded" >
+                  Levy Amount: {{ data.levyAssetAmount }}
+                </div>
+                <div v-tooltip.bottom="data.levyRecipient" class="inline-block bg-blue-100 text-blue-700 font-bold text-txs rounded py-1 px-2 my-1 mr-1 truncate">
+                  Levy Recipient: {{ data.levyRecipient }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </Column>
+      <Column field="hash" header="TX HASH" headerStyle="width:100px" v-if="wideScreen">
         <template #body="{data}">
           <span class="text-txs" v-tooltip.bottom="data.hash">{{data.hash.substring(0, 20) }}...</span>
         </template>
       </Column>
-      <Column field="timestamp" header="TIMESTAMP" v-if="selectedGroupType === transactionGroupType.CONFIRMED" headerStyle="width:110px">
+      <Column field="timestamp" header="TIMESTAMP" v-if="selectedGroupType === transactionGroupType.CONFIRMED && wideScreen" headerStyle="width:110px">
         <template #body="{data}">
           <span class="text-txs">{{ convertLocalTime(data.timestamp) }}</span>
         </template>
       </Column>
-      <Column field="typeName" header="TYPE" headerStyle="width:110px">
+      <Column field="typeName" header="TYPE" headerStyle="width:110px" v-if="wideScreen">
         <template #body="{data}">
           <span class="text-txs">{{data.type}}</span>
         </template>
       </Column>
-      <Column field="block" header="BLOCK" v-if="selectedGroupType === transactionGroupType.CONFIRMED" headerStyle="width:110px">
+      <Column field="block" header="BLOCK" v-if="selectedGroupType === transactionGroupType.CONFIRMED && wideScreen" headerStyle="width:110px">
         <template #body="{data}">
           <div class="text-txs">{{ data.block }}</div>
         </template>
       </Column>
-      <Column header="TX FEE" v-if="selectedGroupType === transactionGroupType.CONFIRMED" headerStyle="width:110px">
+      <Column header="TX FEE" v-if="selectedGroupType === transactionGroupType.CONFIRMED && wideScreen" headerStyle="width:110px">
         <template #body="{data}">
           <div class="text-txs">{{ data.fee }} <b v-if="data.fee">{{ nativeTokenName }}</b></div>
         </template>
       </Column>
 
-      <Column header="ASSET ID" headerStyle="width:110px">
+      <Column header="ASSET ID" headerStyle="width:110px" v-if="wideScreen">
         <template #body="{data}">
           <div class="text-txs">{{ data.assetId }}{{ data.namespaceName ? `(${data.namespaceName})`: "" }}</div>
         </template>
       </Column>
-      <Column header="INFO" headerStyle="width:40px">
+      <Column header="INFO" headerStyle="width:40px" v-if="wideScreen">
         <template #body="{data}">
-          <span class="inline-block bg-blue-500 text-white py-1 px-1 my-1 mx-1" v-if="data.nonce">{{ `Nonce: ${data.nonce}` }}</span>
-          <span class="inline-block bg-blue-500 text-white py-1 px-1 my-1 mx-1" v-if="data.divisibility !== null">{{ `Divisibility: ${data.divisibility}` }}</span>
-          <span v-bind:class="['inline-block','text-black', 'py-1', 'px-1', 'my-1', 'mx-1', (data.transferable) ? 'bg-green-300' : 'bg-red-300']" v-if="data.transferable !== null">Transferable</span>
-          <span v-bind:class="['inline-block','text-black', 'py-1', 'px-1', 'my-1', 'mx-1', (data.supplyMutable) ? 'bg-green-300' : 'bg-red-300']" v-if="data.supplyMutable !== null">Supply Mutable</span>
-          <span class="inline-block bg-blue-500 text-white py-1 px-1 my-1 mx-1" v-if="data.duration">{{ `Duration: ${data.duration}` }}</span>
-          <span class="inline-block bg-blue-500 text-white py-1 px-1 my-1 mx-1" v-if="data.supplyDelta !== null">Supply:{{ data.supplyDelta > 0 ? "+" + data.supplyDelta : data.supplyDelta }}</span>
+          <span class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mx-1 rounded" v-if="data.nonce">{{ `Nonce: ${data.nonce}` }}</span>
+          <span class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mx-1 rounded" v-if="data.divisibility !== null">{{ `Divisibility: ${data.divisibility}` }}</span>
+          <span v-bind:class="['inline-block','text-black', 'py-1', 'px-2', 'my-1', 'mx-1', 'text-txs', 'font-bold', 'rounded', (data.transferable) ? 'bg-green-200 text-green-700' : 'bg-red-200 text-red-700']" v-if="data.transferable !== null">Transferable</span>
+          <span v-bind:class="['inline-block','text-black', 'py-1', 'px-2', 'my-1', 'mx-1', 'text-txs', 'font-bold', 'rounded', (data.supplyMutable) ? 'bg-green-200 text-green-700' : 'bg-red-200 text-red-700']" v-if="data.supplyMutable !== null">Supply Mutable</span>
+          <span class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mx-1 rounded" v-if="data.duration">{{ `Duration: ${data.duration}` }}</span>
+          <span class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mx-1 rounded" v-if="data.supplyDelta !== null">Supply:{{ data.supplyDelta > 0 ? "+" + data.supplyDelta : data.supplyDelta }}</span>
           <span v-if="data.levyAssetId">
-            <span class="inline-block bg-blue-500 text-white py-1 px-1 my-1 mx-1">
+            <span class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mx-1 rounded">
               Levy: {{ data.levyAssetId }}{{ data.levyAssetName ? `(${data.levyAssetName})`: "" }}
             </span>
-            <span class="inline-block bg-blue-500 text-white py-1 px-1 my-1 mx-1" >
+            <span class="inline-block bg-blue-100 text-blue-700 font-bold text-txs py-1 px-2 my-1 mx-1 rounded" >
               Levy Amount: {{ data.levyAssetAmount }}
             </span>
-            <span v-tooltip.bottom="data.levyRecipient" class="inline-block bg-blue-500 text-white py-1 px-1 my-1 mx-1 truncate inline-block">
+            <span v-tooltip.bottom="data.levyRecipient" class="inline-block bg-blue-100 text-blue-700 font-bold text-txs rounded py-1 px-2 my-1 mx-1 truncate">
               Levy Recipient: {{ data.levyRecipient }}
             </span>
           </span>
         </template>
       </Column>
-      <Column header="" headerStyle="width:20px">
+      <Column header="" headerStyle="width:50px">
         <template #body="{data}">
           <img src="@/modules/dashboard/img/icon-open_in_new_black.svg" @click="gotoHashExplorer(data.hash)" class="cursor-pointer">
         </template>
@@ -79,7 +129,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { getCurrentInstance, ref, computed, watch } from "vue";
+import { getCurrentInstance, ref, computed, watch, onMounted, onUnmounted } from "vue";
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import {FilterMatchMode} from 'primevue/api';
@@ -107,6 +157,24 @@ export default defineComponent({
     'tooltip': Tooltip
   },
   setup(p, context){
+    const wideScreen = ref(false);
+    const screenResizeHandler = () => {
+      if(window.innerWidth < 1024){
+        wideScreen.value = false;
+      }else{
+        wideScreen.value = true;
+      }
+    };
+    screenResizeHandler();
+
+    onUnmounted(() => {
+      window.removeEventListener("resize", screenResizeHandler);
+    });
+
+    onMounted(() => {
+      window.addEventListener("resize", screenResizeHandler);
+    });
+
     const internalInstance = getCurrentInstance();
     const emitter = internalInstance.appContext.config.globalProperties.emitter;
     const borderColor = ref('border border-gray-400');
@@ -196,7 +264,9 @@ export default defineComponent({
       nativeTokenName,
       convertLocalTime,
       transactionGroupType,
-      constructSDA
+      constructSDA,
+      wideScreen,
+      Helper,
     }
   }
 })
