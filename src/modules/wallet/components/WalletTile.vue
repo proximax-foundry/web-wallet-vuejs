f4d44865b23a6c30683c8f610fb4265ad05d960a
HAIENG <haieng.chong@proximax.io>
2022-01-26 20:32:29 +0800

    remove duplicate css

--- src/modules/wallet/components/WalletTile.vue
+++ src/modules/wallet/components/WalletTile.vue
@@ -28,7 +28,7 @@
         </button>
       </div>
        <div v-else class="xl:mr-1 xl:ml-5 text-xs font-bold flex">   
-        <button class="font-bold py-2 sm:py-2 md:px-5 md:py-2 md:px-5 xl:py-2 xl:mr-2 xl:ml-0 rounded inline-flex items-center" @click="deleteWallet(wallet.name)">
+        <button class="font-bold py-2 sm:py-2 md:px-5 md:py-2 xl:py-2 xl:mr-2 xl:ml-0 rounded inline-flex items-center" @click="deleteWallet(wallet.name)">
         <svg id="clear_black_24dp" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
           <path id="Path_114" data-name="Path 114" d="M0,0H24V24H0Z" fill="none"/>
           <path id="Path_115" data-name="Path 115" d="M18.3,5.71a1,1,0,0,0-1.41,0L12,10.59,7.11,5.7A1,1,0,0,0,5.7,7.11L10.59,12,5.7,16.89A1,1,0,0,0,7.11,18.3L12,13.41l4.89,4.89a1,1,0,0,0,1.41-1.41L13.41,12,18.3,7.11A1,1,0,0,0,18.3,5.71Z"/>
