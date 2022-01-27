8429f4642693e95ba5f8c4458499f4c42f0e02e3
laics1984 <laics1984@users.noreply.github.com>
2022-01-27 02:05:18 +0000

    deploy: 98ca210299b8912c8d7ea291ad09c0cae9bf74b9

--- assets/js/89.js
+++ assets/js/89.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[89],{
 
-/***/ "./src/modules/dashboard/img/icon-more-options.svg":
-/*!*********************************************************!*\
-  !*** ./src/modules/dashboard/img/icon-more-options.svg ***!
-  \*********************************************************/
+/***/ "./src/modules/account/img/proximax-logo.svg":
+/*!***************************************************!*\
+  !*** ./src/modules/account/img/proximax-logo.svg ***!
+  \***************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icon-more-options.54384d4a.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-more-options.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/proximax-logo.cb656acf.svg\";\n\n//# sourceURL=webpack:///./src/modules/account/img/proximax-logo.svg?");
 
 /***/ })
 
