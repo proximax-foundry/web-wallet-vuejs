2460c88f1073cd51332f0232b533b83c2a9f86ab
jiaming <jiaminglim0828@gmail.com>
2022-02-10 22:39:49 +0800

    fix delete account modal

--- src/modules/account/components/DeleteAccountModal.vue
+++ src/modules/account/components/DeleteAccountModal.vue
@@ -10,7 +10,7 @@
   <div v-if='toggleModal' class="popup-outer fixed flex z-50 text-black mt-10">
     <div class="modal-popup-box">
       <div class="error error_box mb-3 text-center" v-if="err!=''">{{ err }}</div>
-        <div class ='text-center mt-2 text-md font-normal'>Wallet Deletion</div>
+        <div class ='text-center mt-2 text-md font-normal'>Account Deletion</div>
           <img src='@/modules/wallet/img/icon-delete-wallet.svg' class='ml-auto mr-auto'>
           <div class ='text-black text-center text-xs mt-2 font-normal'>This action will delete the account.</div>
           <div class ='font-bold text-center text-xs mt-2'>Are you sure you want to delete this account?</div>
@@ -19,7 +19,7 @@
               <div class="mt-3">
                 <PasswordInput class ='w-8/12 ml-auto mr-auto' placeholder="Password"  v-model="walletPasswd" icon="lock"/>
                 <button @click="deleteAccount()" class='red-btn px-4 py-3 font-semibold cursor-pointer text-center ml-auto mt-3 mr-auto w-8/12 disabled:opacity-50 disabled:cursor-auto' :disabled="disableDelete">Confirm Account Deletion</button>
-                <div class= 'text-center cursor-pointer text-xs font-semibold text-blue-link mt-3' @click="toggleModal = !toggleModal;walletPasswd=''">Cancel</div>
+                <div class= 'text-center cursor-pointer text-xs font-semibold text-blue-link mt-3' @click="toggleModal = !toggleModal;walletPasswd='';err=''">Cancel</div>
               </div>
             </div> 
         </div>
@@ -34,7 +34,7 @@ import { walletState } from '@/state/walletState';
 import { networkState } from '@/state/networkState';
 import { WalletUtils } from '@/util/walletUtils';
 import { Account } from '@/models/account';
-import {  ref} from "vue";
+import {  ref,computed} from "vue";
 import {useI18n} from 'vue-i18n'
 import { useRouter } from "vue-router";
 export default {
@@ -50,6 +50,8 @@ export default {
       const router = useRouter();
       let toggleModal = ref(false)
       let walletPasswd = ref('')
+      const passwdPattern = "^[^ ]{8,}$";
+      const disableDelete = computed(() => !(walletPasswd.value.match(passwdPattern)));
       let err = ref('')
       const deleteAccount = () => {
       const accountIndex = walletState.currentLoggedInWallet.accounts.findIndex((element) => element.name == p.account.name);
@@ -70,7 +72,8 @@ export default {
       deleteAccount,
       toggleModal,
       err,
-      walletPasswd
+      walletPasswd,
+      disableDelete
     }
     }
 }
