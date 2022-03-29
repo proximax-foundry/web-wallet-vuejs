08c35a45ed6bf6e727faebfcf3c4e4a3e5ddf28c
anakin001 <anakin001@users.noreply.github.com>
2022-03-29 05:29:39 +0000

    deploy: 3cd42b19dce35cb576b14e481d5ba76e6185e930

--- assets/js/78.js
+++ assets/js/78.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[78],{
 
-/***/ "./src/modules/services/submodule/mainnetSwap/img/bsc.svg":
-/*!****************************************************************!*\
-  !*** ./src/modules/services/submodule/mainnetSwap/img/bsc.svg ***!
-  \****************************************************************/
+/***/ "./src/modules/dashboard/img/icon-more-options.svg":
+/*!*********************************************************!*\
+  !*** ./src/modules/dashboard/img/icon-more-options.svg ***!
+  \*********************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/bsc.6b130f35.svg\";\n\n//# sourceURL=webpack:///./src/modules/services/submodule/mainnetSwap/img/bsc.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-more-options.54384d4a.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-more-options.svg?");
 
 /***/ })
 
