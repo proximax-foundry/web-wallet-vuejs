3166c984ec583ef6421a83cbde396eb6ab1660e5
laics1984 <laics1984@users.noreply.github.com>
2022-04-20 01:23:44 +0000

    deploy: 895dd76094b91b7dca0be84b90fd7a18615e9f5c

--- assets/js/56.js
+++ assets/js/56.js
@@ -1,29 +1,5 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[56],{
 
-/***/ "./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader-v16/dist/index.js?!./src/components/IntroTextComponent.vue?vue&type=script&lang=js":
-/*!**********************************************************************************************************************************************************************!*\
-  !*** ./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1!./src/components/IntroTextComponent.vue?vue&type=script&lang=js ***!
-  \**********************************************************************************************************************************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  name: 'IntroTextComponent',\n});\n\n\n//# sourceURL=webpack:///./src/components/IntroTextComponent.vue?./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1");
-
-/***/ }),
-
-/***/ "./node_modules/vue-loader-v16/dist/templateLoader.js?!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader-v16/dist/index.js?!./src/components/IntroTextComponent.vue?vue&type=template&id=2a76d374":
-/*!*****************************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/vue-loader-v16/dist/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1!./src/components/IntroTextComponent.vue?vue&type=template&id=2a76d374 ***!
-  \*****************************************************************************************************************************************************************************************************************************************/
-/*! exports provided: render */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ \"./node_modules/vue/dist/vue.runtime.esm-bundler.js\");\n\n\nconst _hoisted_1 = { class: \"md:col-span-1 p-5 sm:pt-20 text-tsm md:text-sm text-gray-700 md:text-right xl:mr-32\" }\nconst _hoisted_2 = { class: \"text-sm my-2 md:text-xl md:my-7 text-white\" }\nconst _hoisted_3 = { class: \"xl:w-72 text-white md:inline-block\" }\n\nfunction render(_ctx, _cache, $props, $setup, $data, $options) {\n  return (Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"openBlock\"])(), Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"createElementBlock\"])(\"div\", _hoisted_1, [\n    Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"createElementVNode\"])(\"div\", _hoisted_2, Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"toDisplayString\"])(_ctx.$t('home.title')), 1 /* TEXT */),\n    Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"createElementVNode\"])(\"div\", _hoisted_3, Object(vue__WEBPACK_IMPORTED_MODULE_0__[\"toDisplayString\"])(_ctx.$t('home.titleDescription')), 1 /* TEXT */)\n  ]))\n}\n\n//# sourceURL=webpack:///./src/components/IntroTextComponent.vue?./node_modules/vue-loader-v16/dist/templateLoader.js??ref--6!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader-v16/dist??ref--0-1");
-
-/***/ }),
-
 /***/ "./src/assets/img/chevron_left.svg":
 /*!*****************************************!*\
   !*** ./src/assets/img/chevron_left.svg ***!
@@ -35,39 +11,70 @@ eval("module.exports = __webpack_require__.p + \"assets/img/chevron_left.120f1ae
 
 /***/ }),
 
-/***/ "./src/components/IntroTextComponent.vue":
+/***/ "./src/assets/img/icon-key.svg":
+/*!*************************************!*\
+  !*** ./src/assets/img/icon-key.svg ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-key.0e181e5e.svg\";\n\n//# sourceURL=webpack:///./src/assets/img/icon-key.svg?");
+
+/***/ }),
+
+/***/ "./src/modules/account/img/icon-pin.svg":
+/*!**********************************************!*\
+  !*** ./src/modules/account/img/icon-pin.svg ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-pin.4428bcf6.svg\";\n\n//# sourceURL=webpack:///./src/modules/account/img/icon-pin.svg?");
+
+/***/ }),
+
+/***/ "./src/modules/account/img/metx-logo.svg":
 /*!***********************************************!*\
-  !*** ./src/components/IntroTextComponent.vue ***!
+  !*** ./src/modules/account/img/metx-logo.svg ***!
   \***********************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _IntroTextComponent_vue_vue_type_template_id_2a76d374__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./IntroTextComponent.vue?vue&type=template&id=2a76d374 */ \"./src/components/IntroTextComponent.vue?vue&type=template&id=2a76d374\");\n/* harmony import */ var _IntroTextComponent_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./IntroTextComponent.vue?vue&type=script&lang=js */ \"./src/components/IntroTextComponent.vue?vue&type=script&lang=js\");\n/* empty/unused harmony star reexport *//* harmony import */ var _home_runner_work_web_wallet_vuejs_web_wallet_vuejs_node_modules_vue_loader_v16_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader-v16/dist/exportHelper.js */ \"./node_modules/vue-loader-v16/dist/exportHelper.js\");\n/* harmony import */ var _home_runner_work_web_wallet_vuejs_web_wallet_vuejs_node_modules_vue_loader_v16_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_home_runner_work_web_wallet_vuejs_web_wallet_vuejs_node_modules_vue_loader_v16_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\n\nconst __exports__ = /*#__PURE__*/_home_runner_work_web_wallet_vuejs_web_wallet_vuejs_node_modules_vue_loader_v16_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2___default()(_IntroTextComponent_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"], [['render',_IntroTextComponent_vue_vue_type_template_id_2a76d374__WEBPACK_IMPORTED_MODULE_0__[\"render\"]],['__file',\"src/components/IntroTextComponent.vue\"]])\n/* hot reload */\nif (false) {}\n\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (__exports__);\n\n//# sourceURL=webpack:///./src/components/IntroTextComponent.vue?");
-
-/***/ }),
-
-/***/ "./src/components/IntroTextComponent.vue?vue&type=script&lang=js":
-/*!***********************************************************************!*\
-  !*** ./src/components/IntroTextComponent.vue?vue&type=script&lang=js ***!
-  \***********************************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_IntroTextComponent_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/cache-loader/dist/cjs.js??ref--0-0!../../node_modules/vue-loader-v16/dist??ref--0-1!./IntroTextComponent.vue?vue&type=script&lang=js */ \"./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader-v16/dist/index.js?!./src/components/IntroTextComponent.vue?vue&type=script&lang=js\");\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return _node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_IntroTextComponent_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"]; });\n\n/* empty/unused harmony star reexport */ \n\n//# sourceURL=webpack:///./src/components/IntroTextComponent.vue?");
+eval("module.exports = __webpack_require__.p + \"assets/img/metx-logo.a478365d.svg\";\n\n//# sourceURL=webpack:///./src/modules/account/img/metx-logo.svg?");
 
 /***/ }),
 
-/***/ "./src/components/IntroTextComponent.vue?vue&type=template&id=2a76d374":
-/*!*****************************************************************************!*\
-  !*** ./src/components/IntroTextComponent.vue?vue&type=template&id=2a76d374 ***!
-  \*****************************************************************************/
-/*! exports provided: render */
+/***/ "./src/modules/account/img/proximax-logo.svg":
+/*!***************************************************!*\
+  !*** ./src/modules/account/img/proximax-logo.svg ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/proximax-logo.cb656acf.svg\";\n\n//# sourceURL=webpack:///./src/modules/account/img/proximax-logo.svg?");
+
+/***/ }),
+
+/***/ "./src/modules/dashboard/img/icon-sda.svg":
+/*!************************************************!*\
+  !*** ./src/modules/dashboard/img/icon-sda.svg ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-sda.5be71878.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-sda.svg?");
+
+/***/ }),
+
+/***/ "./src/util/functions.ts":
+/*!*******************************!*\
+  !*** ./src/util/functions.ts ***!
+  \*******************************/
+/*! exports provided: copyKeyFunc, copyToClipboard, getCoingeckoCoinPrice, getCurrentPriceUSD, getXPXcurrencyPrice */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_vue_loader_v16_dist_templateLoader_js_ref_6_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_IntroTextComponent_vue_vue_type_template_id_2a76d374__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/vue-loader-v16/dist/templateLoader.js??ref--6!../../node_modules/cache-loader/dist/cjs.js??ref--0-0!../../node_modules/vue-loader-v16/dist??ref--0-1!./IntroTextComponent.vue?vue&type=template&id=2a76d374 */ \"./node_modules/vue-loader-v16/dist/templateLoader.js?!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader-v16/dist/index.js?!./src/components/IntroTextComponent.vue?vue&type=template&id=2a76d374\");\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return _node_modules_vue_loader_v16_dist_templateLoader_js_ref_6_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_v16_dist_index_js_ref_0_1_IntroTextComponent_vue_vue_type_template_id_2a76d374__WEBPACK_IMPORTED_MODULE_0__[\"render\"]; });\n\n\n\n//# sourceURL=webpack:///./src/components/IntroTextComponent.vue?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"copyKeyFunc\", function() { return copyKeyFunc; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"copyToClipboard\", function() { return copyToClipboard; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"getCoingeckoCoinPrice\", function() { return getCoingeckoCoinPrice; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"getCurrentPriceUSD\", function() { return getCurrentPriceUSD; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"getXPXcurrencyPrice\", function() { return getXPXcurrencyPrice; });\n/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ \"./node_modules/tslib/tslib.es6.js\");\n/* harmony import */ var primevue_usetoast__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! primevue/usetoast */ \"./node_modules/primevue/usetoast/usetoast.esm.js\");\n\n\n// copy address keys\nconst copyKeyFunc = (id) => {\n    const toast = Object(primevue_usetoast__WEBPACK_IMPORTED_MODULE_1__[\"useToast\"])();\n    // Credits: https://www.w3schools.com/howto/howto_js_copy_clipboard.asp\n    let copyText = document.getElementById(id);\n    /* Select the text field */\n    //copyText.innerText;\n    copyText === null || copyText === void 0 ? void 0 : copyText.select();\n    copyText === null || copyText === void 0 ? void 0 : copyText.setSelectionRange(0, 99999); /* For mobile devices */\n    /* Copy the text inside the text field */\n    document.execCommand(\"copy\");\n    let titleType;\n    if (id == 'private') {\n        titleType = 'Private key';\n    }\n    else if (id == 'public') {\n        titleType = 'Public key';\n    }\n    else if (id == 'pollIndexAddress') {\n        titleType = 'Poll Index Address';\n    }\n    else {\n        titleType = 'Address';\n    }\n    // toast.add({severity:'info', summary: titleType + ' copied', detail: copyText.value, group: 'br', life: 5000});\n};\nconst copyToClipboard = (data) => {\n    /*const listener = (e: ClipboardEvent) => {\n      e.clipboardData.setData('text/plain', data);\n      e.preventDefault();\n      document.removeEventListener('copy', listener);\n    };\n    document.addEventListener('copy', listener);\n    document.execCommand('copy');*/\n    let textInput;\n    try {\n        textInput = document.createElement('textarea');\n        textInput.setAttribute('readonly', true);\n        textInput.setAttribute('contenteditable', true);\n        textInput.style.position = 'fixed';\n        textInput.value = data;\n        document.body.appendChild(textInput);\n        textInput.focus();\n        textInput.select();\n        const range = document.createRange();\n        range.selectNodeContents(textInput);\n        const textSelection = window.getSelection();\n        textSelection.removeAllRanges();\n        textSelection.addRange(range);\n        textInput.setSelectionRange(0, textInput.value.length);\n        document.execCommand('copy');\n    }\n    catch (err) {\n        console.error(err);\n    }\n    finally {\n        document.body.removeChild(textInput);\n    }\n    return true;\n};\nconst getCoingecko = (coinId) => {\n    return fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`).then((res) => res.json()).then((data) => { return data.market_data.current_price.usd; });\n};\nconst getCoingeckoCoinPrice = (coinId) => {\n    return fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`).then((res) => res.json()).then((data) => { return data.market_data.current_price; });\n};\nconst getCurrentPriceUSD = (url) => {\n    return fetch(url).then((res) => res.json()).then((data) => { return data; });\n};\nconst getXPXcurrencyPrice = (balance) => Object(tslib__WEBPACK_IMPORTED_MODULE_0__[\"__awaiter\"])(void 0, void 0, void 0, function* () {\n    let total;\n    let coinId = 'proximax';\n    let rate = yield getCoingecko(coinId);\n    total = rate * balance;\n    return total;\n});\n// export const getPrettyAddress = (address: string) => {\n//   const prettyAddress = Address.createFromRawAddress(address);\n//   return prettyAddress.pretty();\n// }\n\n\n//# sourceURL=webpack:///./src/util/functions.ts?");
 
 /***/ })
 
