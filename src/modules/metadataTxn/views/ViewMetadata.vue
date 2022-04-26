c1f1ba64f7d27f1f99921cf7e76b6814f095d198
jiaming <jiaminglim0828@gmail.com>
2022-04-26 17:36:42 +0800

    view metadata UI optimize

--- src/modules/metadataTxn/views/ViewMetadata.vue
+++ src/modules/metadataTxn/views/ViewMetadata.vue
@@ -22,88 +22,100 @@
                     <option value=2>ASSET</option>
                 </select>
                 <div v-if="filterSelection==0">
-                    <div class="grid grid-cols-2 text-gray-400 font-semibold text-xs uppercase mb-2">
-                        <div>Scoped Metadata Key</div>
-                        <div>Current Value</div>
-                    </div>
-                    <div v-for="(metadata,index) in accountMetadata" :key="index">
-                         <div class="grid grid-cols-2 text-xs mb-2">
-                            <div>
-                                <div class="flex">
-                                    <div>{{metadata.scopedMetadataKeyHex}}</div>
-                                    <div class="ml-3 text-gray-400 font-semibold" v-if="metadata.scopedMetadataKeyUtf8">hex</div>
-                                </div>
-                                <div class="flex" v-if="metadata.scopedMetadataKeyUtf8">
-                                    <div>{{metadata.scopedMetadataKeyUtf8}}</div>
-                                    <div class="ml-3 text-gray-400 font-semibold">utf-8</div>
-                                </div>
-                            </div>
-                            <div class="break-all">
-                                {{metadata.value}} 
-                                <router-link :to="{name: 'ViewUpdateAccountMetadata',params:{targetPublicKey:acc?acc.publicKey:'0'.repeat(64),scopedMetadataKey:metadata.scopedMetadataKeyUtf8?metadata.scopedMetadataKeyUtf8:metadata.scopedMetadataKeyHex}}">
-                                    <img src="@/modules/account/img/edit-icon.svg" title="Update Metadata" class="inline-block w-3 h-3 text-black cursor-pointer  ml-1" >
-                                </router-link>
-                            </div>
+                    <div v-if="!accountMetadata.length" class='text-blue-primary text-xs text-center font-semibold'>{{$t('general.ntgToShow')}}</div>
+                    <div class="text-txs text-gray-400 my-1 text-center" v-if="!accountMetadata.length">You do not have any existing metadatas on record.</div>
+                    <div v-else>
+                        <div class="grid grid-cols-2 text-gray-400 font-semibold text-xs uppercase mb-2">
+                            <div>Scoped Metadata Key</div>
+                            <div>Current Value</div>
                         </div>
-                        <div v-if="index != (accountMetadata.length - 1)" class='my-2 gray-line' ></div>
+                        <div v-for="(metadata,index) in accountMetadata" :key="index">
+                            <div class="grid grid-cols-2 text-xs mb-2">
+                                <div>
+                                    <div class="flex">
+                                        <div>{{metadata.scopedMetadataKeyHex}}</div>
+                                        <div class="ml-3 text-gray-400 font-semibold" v-if="metadata.scopedMetadataKeyUtf8">hex</div>
+                                    </div>
+                                    <div class="flex" v-if="metadata.scopedMetadataKeyUtf8">
+                                        <div>{{metadata.scopedMetadataKeyUtf8}}</div>
+                                        <div class="ml-3 text-gray-400 font-semibold">utf-8</div>
+                                    </div>
+                                </div>
+                                <div class="break-all">
+                                    {{metadata.value}} 
+                                    <router-link :to="{name: 'ViewUpdateAccountMetadata',params:{targetPublicKey:acc?acc.publicKey:'0'.repeat(64),scopedMetadataKey:metadata.scopedMetadataKeyUtf8?metadata.scopedMetadataKeyUtf8:metadata.scopedMetadataKeyHex}}">
+                                        <img src="@/modules/account/img/edit-icon.svg" title="Update Metadata" class="inline-block w-3 h-3 text-black cursor-pointer  ml-1" >
+                                    </router-link>
+                                </div>
+                            </div>
+                            <div v-if="index != (accountMetadata.length - 1)" class='my-2 gray-line' ></div>
+                        </div>
                     </div>
                 </div>
                 <div v-else-if="filterSelection==1">
-                     <div class="grid grid-cols-3 text-gray-400 font-semibold text-xs uppercase mb-2">
-                        <div>Scoped Metadata Key</div>
-                        <div>Target ID</div>
-                        <div>Current Value</div>
-                    </div>
-                    <div v-for="(metadata,index) in namespaceMetadata" :key="index">
-                        <div class="grid grid-cols-3 text-xs mb-2">
-                            <div>
-                                <div class="flex">
-                                    <div>{{metadata.scopedMetadataKeyHex}}</div>
-                                    <div class="ml-3 text-gray-400 font-semibold" v-if="metadata.scopedMetadataKeyUtf8">hex</div>
-                                </div>
-                                <div class="flex" v-if="metadata.scopedMetadataKeyUtf8">
-                                    <div>{{metadata.scopedMetadataKeyUtf8}}</div>
-                                    <div class="ml-3 text-gray-400 font-semibold">utf-8</div>
-                                </div>
-                            </div>
-                            <div>{{metadata.id}}</div>
-                            <div class="break-all">
-                                {{metadata.value}}
-                                 <router-link :to="{name: 'ViewUpdateNamespaceMetadata',params:{targetId:metadata.id,scopedMetadataKey:metadata.scopedMetadataKeyUtf8?metadata.scopedMetadataKeyUtf8:metadata.scopedMetadataKeyHex}}">
-                                    <img src="@/modules/account/img/edit-icon.svg" title="Update Metadata" class="inline-block w-3 h-3 text-black cursor-pointer  ml-1" >
-                                </router-link>
-                            </div>
+                    <div v-if="!namespaceMetadata.length" class='text-blue-primary text-xs text-center font-semibold'>{{$t('general.ntgToShow')}}</div>
+                    <div class="text-txs text-gray-400 my-1 text-center" v-if="!namespaceMetadata.length">You do not have any existing metadatas on record.</div>
+                    <div v-else>
+                        <div class="grid grid-cols-3 text-gray-400 font-semibold text-xs uppercase mb-2">
+                            <div>Scoped Metadata Key</div>
+                            <div>Target ID</div>
+                            <div>Current Value</div>
                         </div>
-                        <div v-if="index != (namespaceMetadata.length - 1)" class='my-2 gray-line' ></div>
+                        <div v-for="(metadata,index) in namespaceMetadata" :key="index">
+                            <div class="grid grid-cols-3 text-xs mb-2">
+                                <div>
+                                    <div class="flex">
+                                        <div>{{metadata.scopedMetadataKeyHex}}</div>
+                                        <div class="ml-3 text-gray-400 font-semibold" v-if="metadata.scopedMetadataKeyUtf8">hex</div>
+                                    </div>
+                                    <div class="flex" v-if="metadata.scopedMetadataKeyUtf8">
+                                        <div>{{metadata.scopedMetadataKeyUtf8}}</div>
+                                        <div class="ml-3 text-gray-400 font-semibold">utf-8</div>
+                                    </div>
+                                </div>
+                                <div>{{metadata.id}}</div>
+                                <div class="break-all">
+                                    {{metadata.value}}
+                                    <router-link :to="{name: 'ViewUpdateNamespaceMetadata',params:{targetId:metadata.id,scopedMetadataKey:metadata.scopedMetadataKeyUtf8?metadata.scopedMetadataKeyUtf8:metadata.scopedMetadataKeyHex}}">
+                                        <img src="@/modules/account/img/edit-icon.svg" title="Update Metadata" class="inline-block w-3 h-3 text-black cursor-pointer  ml-1" >
+                                    </router-link>
+                                </div>
+                            </div>
+                            <div v-if="index != (namespaceMetadata.length - 1)" class='my-2 gray-line' ></div>
+                        </div>
                     </div>
                 </div>
                 <div v-else>
-                     <div class="grid grid-cols-3 text-gray-400 font-semibold text-xs uppercase mb-2">
-                        <div>Scoped Metadata Key</div>
-                        <div>Target ID</div>
-                        <div>Current Value</div>
-                    </div>
-                    <div v-for="(metadata,index) in assetMetadata" :key="index">
-                        <div class="grid grid-cols-3 text-xs mb-2">
-                            <div>
-                                <div class="flex">
-                                    <div>{{metadata.scopedMetadataKeyHex}}</div>
-                                    <div class="ml-3 text-gray-400 font-semibold" v-if="metadata.scopedMetadataKeyUtf8">hex</div>
-                                </div>
-                                <div class="flex" v-if="metadata.scopedMetadataKeyUtf8">
-                                    <div>{{metadata.scopedMetadataKeyUtf8}}</div>
-                                    <div class="ml-3 text-gray-400 font-semibold">utf-8</div>
-                                </div>
-                            </div>
-                            <div>{{metadata.name}}</div>
-                            <div class="break-all">
-                                {{metadata.value}}
-                                <router-link :to="{name: 'ViewUpdateAssetMetadata',params:{targetId:metadata.id,scopedMetadataKey:metadata.scopedMetadataKeyUtf8?metadata.scopedMetadataKeyUtf8:metadata.scopedMetadataKeyHex}}">
-                                    <img src="@/modules/account/img/edit-icon.svg" title="Update Metadata" class="inline-block w-3 h-3 text-black cursor-pointer  ml-1" >
-                                </router-link>
-                            </div>
+                    <div v-if="!assetMetadata.length" class='text-blue-primary text-xs text-center font-semibold'>{{$t('general.ntgToShow')}}</div>
+                    <div class="text-txs text-gray-400 my-1 text-center" v-if="!assetMetadata.length">You do not have any existing metadatas on record.</div>
+                    <div v-else>
+                        <div class="grid grid-cols-3 text-gray-400 font-semibold text-xs uppercase mb-2">
+                            <div>Scoped Metadata Key</div>
+                            <div>Target ID</div>
+                            <div>Current Value</div>
                         </div>
-                        <div v-if="index != (assetMetadata.length - 1)" class='my-2 gray-line' ></div>
+                        <div v-for="(metadata,index) in assetMetadata" :key="index">
+                            <div class="grid grid-cols-3 text-xs mb-2">
+                                <div>
+                                    <div class="flex">
+                                        <div>{{metadata.scopedMetadataKeyHex}}</div>
+                                        <div class="ml-3 text-gray-400 font-semibold" v-if="metadata.scopedMetadataKeyUtf8">hex</div>
+                                    </div>
+                                    <div class="flex" v-if="metadata.scopedMetadataKeyUtf8">
+                                        <div>{{metadata.scopedMetadataKeyUtf8}}</div>
+                                        <div class="ml-3 text-gray-400 font-semibold">utf-8</div>
+                                    </div>
+                                </div>
+                                <div>{{metadata.name}}</div>
+                                <div class="break-all">
+                                    {{metadata.value}}
+                                    <router-link :to="{name: 'ViewUpdateAssetMetadata',params:{targetId:metadata.id,scopedMetadataKey:metadata.scopedMetadataKeyUtf8?metadata.scopedMetadataKeyUtf8:metadata.scopedMetadataKeyHex}}">
+                                        <img src="@/modules/account/img/edit-icon.svg" title="Update Metadata" class="inline-block w-3 h-3 text-black cursor-pointer  ml-1" >
+                                    </router-link>
+                                </div>
+                            </div>
+                            <div v-if="index != (assetMetadata.length - 1)" class='my-2 gray-line' ></div>
+                        </div>
                     </div>
                 </div>
                 <router-link :to="{name: 'ViewUpdateAccountMetadata',params:{targetPublicKey:acc?acc.publicKey:'0'.repeat(64)}}"><button  class="my-4 blue-btn py-3 px-3 " >Create New Account Metadata</button></router-link>
