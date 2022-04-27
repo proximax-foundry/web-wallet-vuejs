0225e550f9725cecfb64cc7e4efc3936d6310f0b
jiaming <jiaminglim0828@gmail.com>
2022-04-27 18:11:28 +0800

    restrict input

--- src/modules/metadataTxn/components/MetadataInput.vue
+++ src/modules/metadataTxn/components/MetadataInput.vue
@@ -2,7 +2,7 @@
   <div :class="disabled?'opacity-50':''">
     <div class="border border-gray-200 px-2 py-1 h-14 rounded-md">
       <div class="uppercase text-gray-500 text-txs text-left mb-2">{{ placeholder }}<img src="@/assets/img/icon-info.svg" class="inline-block ml-1 relative cursor-pointer" style="top: -1px;" v-tooltip.bottom="'<tiptext>' + toolTip + '</tiptext>'" v-if="toolTip"></div>
-      <input v-if="!hex" class="number_input" :disabled="disabled" @input="$emit('update:modelValue', $event.target.value)" :value="modelValue" type="text"  @focus="$event.target.select()">
+      <input v-if="!hex" class="number_input" :disabled="disabled" @input="$emit('update:modelValue', $event.target.value)" :value="modelValue" type="text"  @focus="$event.target.select()" v-on:keypress="intCharOnly(event)" >
       <input v-if="hex" type="text" class="number_input" :disabled="disabled" @input="$emit('update:modelValue', $event.target.value)" :value="modelValue" @focus="$event.target.select()" v-on:keypress="hexOnly(event)" >
     </div>
     <div v-if="textErr || showError" class="h-3 mb-2"><div class="error error-text text-left" >{{ errorMessage }}</div></div>
@@ -26,7 +26,7 @@ export default{
     hex: Boolean
   },
   methods:{
-  hexOnly: function(evt) {
+    hexOnly: function(evt) {
       evt = (evt) ? evt : window.event;
       var charCode = (evt.which) ? evt.which : evt.keyCode;
       if ((charCode > 31 && (charCode < 48 || charCode > 57)  && (charCode < 65 || charCode > 70) && (charCode < 97 || charCode > 102))  ) {
@@ -34,7 +34,16 @@ export default{
       } else {
         return 
       }
-    }
+    },
+    intCharOnly: function(evt){
+      evt = (evt) ? evt : window.event;
+      var charCode = (evt.which) ? evt.which : evt.keyCode;
+      if ((charCode > 31 && (charCode < 48 || charCode > 57)  && (charCode < 65 || charCode > 90) && (charCode < 97 || charCode > 122))  ) {
+        evt.preventDefault();
+      } else {
+        return 
+      }
+    },
   },
   emits:[
     'update:modelValue'
