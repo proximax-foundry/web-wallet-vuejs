0a545bf827f3e1140a07cbeedfb8afaa56566f29
jiaming <jiaminglim0828@gmail.com>
2022-05-01 19:04:14 +0800

    allow special char from keyboard input

--- src/modules/metadataTxn/components/MetadataInput.vue
+++ src/modules/metadataTxn/components/MetadataInput.vue
@@ -38,7 +38,7 @@ export default{
     intCharOnly: function(evt){
       evt = (evt) ? evt : window.event;
       var charCode = (evt.which) ? evt.which : evt.keyCode;
-      if ((charCode > 31 && (charCode < 48 || charCode > 57)  && (charCode < 65 || charCode > 90) && (charCode < 97 || charCode > 122))  ) {
+      if (!(charCode > 31 && charCode < 127 )){
         evt.preventDefault();
       } else {
         return 
