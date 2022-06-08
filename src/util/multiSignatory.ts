6f3300610d50d714909b447c7ff7aaa7cd433b67
Jia Ming <62232054+anakin001@users.noreply.github.com>
2022-06-09 01:03:42 +0800

    eye icon revert, fix multisig contact name,fix asset link to namespace bug (#510)

--- src/util/multiSignatory.ts
+++ src/util/multiSignatory.ts
@@ -51,7 +51,7 @@ function generateContact(selected :string,name: string) :{value: string,label:st
     }else{
       contact.push({
         value: element.publicKey,
-        label: element.name + ' - Owner account',
+        label: wallet.convertAddressToName(element.address,true)+ ' - Owner account',
       });
     }
       
