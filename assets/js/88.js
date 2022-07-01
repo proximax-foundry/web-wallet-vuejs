0ab81ebd9a250809aa8b632fa847c69a4cfbb7a1
anakin001 <anakin001@users.noreply.github.com>
2022-07-01 07:45:35 +0000

    deploy: 7ef55bfb9c2e40f7d40a3c4713717061dad5f00d

---
+++ assets/js/88.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[88],{
+
+/***/ "./src/modules/account/img/proximax-logo.svg":
+/*!***************************************************!*\
+  !*** ./src/modules/account/img/proximax-logo.svg ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"assets/img/proximax-logo.cb656acf.svg\";\n\n//# sourceURL=webpack:///./src/modules/account/img/proximax-logo.svg?");
+
+/***/ })
+
+}]);
