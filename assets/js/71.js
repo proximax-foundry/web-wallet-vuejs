1165f2c28878a3bfba139ceb7c5b88fe75168352
sleepyOwl14 <sleepyOwl14@users.noreply.github.com>
2022-07-11 13:11:21 +0000

    deploy: d143686e02c5e1481b4d36086d3ff02e1867ebb3

--- assets/js/71.js
+++ assets/js/71.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[71],{
 
-/***/ "./src/modules/services/submodule/mainnetSwap/img/bsc.svg":
-/*!****************************************************************!*\
-  !*** ./src/modules/services/submodule/mainnetSwap/img/bsc.svg ***!
-  \****************************************************************/
+/***/ "./src/modules/services/submodule/assets/img/icon-asset.svg":
+/*!******************************************************************!*\
+  !*** ./src/modules/services/submodule/assets/img/icon-asset.svg ***!
+  \******************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/bsc.6b130f35.svg\";\n\n//# sourceURL=webpack:///./src/modules/services/submodule/mainnetSwap/img/bsc.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-asset.40e1e8d4.svg\";\n\n//# sourceURL=webpack:///./src/modules/services/submodule/assets/img/icon-asset.svg?");
 
 /***/ })
 
--- assets/js/72.js
+++ assets/js/72.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[72],{
 
-/***/ "./src/modules/services/submodule/namespaces/img/icon-namespace.svg":
-/*!**************************************************************************!*\
-  !*** ./src/modules/services/submodule/namespaces/img/icon-namespace.svg ***!
-  \**************************************************************************/
+/***/ "./src/modules/services/submodule/mainnetSwap/img/bsc.svg":
+/*!****************************************************************!*\
+  !*** ./src/modules/services/submodule/mainnetSwap/img/bsc.svg ***!
+  \****************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icon-namespace.8f06d34d.svg\";\n\n//# sourceURL=webpack:///./src/modules/services/submodule/namespaces/img/icon-namespace.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/bsc.6b130f35.svg\";\n\n//# sourceURL=webpack:///./src/modules/services/submodule/mainnetSwap/img/bsc.svg?");
 
 /***/ })
 
