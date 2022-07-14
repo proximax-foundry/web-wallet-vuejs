737ce2711613498c9868565ffb71c9a96701f23a
anakin001 <anakin001@users.noreply.github.com>
2022-07-14 14:11:34 +0000

    deploy: 6e068abbc0b4bdd721232631aa2bde27fc604f23

--- assets/js/67.js
+++ assets/js/67.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[67],{
 
-/***/ "./src/modules/dashboard/img/icon-xpx.svg":
-/*!************************************************!*\
-  !*** ./src/modules/dashboard/img/icon-xpx.svg ***!
-  \************************************************/
+/***/ "./src/modules/services/submodule/namespaces/img/icon-namespace.svg":
+/*!**************************************************************************!*\
+  !*** ./src/modules/services/submodule/namespaces/img/icon-namespace.svg ***!
+  \**************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icon-xpx.853b72ba.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-xpx.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-namespace.8f06d34d.svg\";\n\n//# sourceURL=webpack:///./src/modules/services/submodule/namespaces/img/icon-namespace.svg?");
 
 /***/ })
 
