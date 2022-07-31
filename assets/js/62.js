93bcbb0750fab435671ae51cb009d2d688e6cfc1
anakin001 <anakin001@users.noreply.github.com>
2022-07-31 14:51:36 +0000

    deploy: 5c9b393090a21216fe0c4842a27224bffaf1339f

--- assets/js/62.js
+++ assets/js/62.js
@@ -1,13 +1,13 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[62],{
 
-/***/ "./src/assets/img/icon-info.svg":
-/*!**************************************!*\
-  !*** ./src/assets/img/icon-info.svg ***!
-  \**************************************/
+/***/ "./src/modules/dashboard/img/icon-more-options.svg":
+/*!*********************************************************!*\
+  !*** ./src/modules/dashboard/img/icon-more-options.svg ***!
+  \*********************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icon-info.c16cc71f.svg\";\n\n//# sourceURL=webpack:///./src/assets/img/icon-info.svg?");
+eval("module.exports = __webpack_require__.p + \"assets/img/icon-more-options.54384d4a.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-more-options.svg?");
 
 /***/ })
 
