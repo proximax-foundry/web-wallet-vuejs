bd33bcd4cf086c1f8f43dc3af10cfa9e3adc10df
Seh Keat <sehkeat1996@gmail.com>
2022-09-09 16:37:05 +0800

    changes

--- src/modules/services/submodule/namespaces/views/ViewServicesNamespaceCreate.vue
+++ src/modules/services/submodule/namespaces/views/ViewServicesNamespaceCreate.vue
@@ -40,8 +40,7 @@
           <span class='ml-auto' v-html="splitCurrency(balance)"></span>
           <img src="@/modules/account/img/proximax-logo.svg" class='ml-1 h-5 w-5 mt-0.5'>
         </div>
-        <div v-if="!isMultiSig(selectedAccAdd)" class='border-b-2 border-gray-600 my-2'/>
-        <div v-else class='border-b-2 border-gray-600 mt-2'/>
+        <div class='border-b-2 border-gray-600 mt-2'/>
         <div class="flex justify-between items-center text-gray-200 text-xs pt-2">
             <div class="font-semibold">{{$t('general.namespacerentalFee')}}</div>
             <div v-html="splitCurrency(rentalFeeCurrency)"></div>
