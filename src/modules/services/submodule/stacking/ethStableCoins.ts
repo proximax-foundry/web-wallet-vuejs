55fab8a7d6e084a943a98558d6ca74b01be506d2
Chun Lam <31641877+sleepyOwl14@users.noreply.github.com>
2023-01-31 00:56:06 +0800

    Fix stablecoin swap disabled token and mainnet detection (#700)
    * fix disabled swap stablecoin
    
    * fix mainnet swap chain id

--- src/modules/services/submodule/stacking/ethStableCoins.ts
+++ src/modules/services/submodule/stacking/ethStableCoins.ts
@@ -1,6 +1,6 @@
 export const ethStableCoins = [
-  { name: 'USDT', contractAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7", decimals: 6, disabled: false },
-  { name: 'USDC', contractAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", decimals: 6, disabled: false },
-  { name: 'BUSD', contractAddress: "0x4Fabb145d64652a948d72533023f6E7A623C7C53", decimals: 18, disabled: false },
-  { name: 'USDP', contractAddress: "0x8e870d67f660d95d5be530380d0ec0bd388289e1", decimals: 18, disabled: false }
+  { name: 'USDT', contractAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7", decimals: 6, disabled: true },
+  { name: 'USDC', contractAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", decimals: 6, disabled: true },
+  { name: 'BUSD', contractAddress: "0x4Fabb145d64652a948d72533023f6E7A623C7C53", decimals: 18, disabled: true },
+  { name: 'USDP', contractAddress: "0x8e870d67f660d95d5be530380d0ec0bd388289e1", decimals: 18, disabled: true }
 ];
