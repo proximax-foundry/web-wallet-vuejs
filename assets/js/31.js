a8bb2f50465619124bb6d412f3acb2a12b2366c7
anakin001 <anakin001@users.noreply.github.com>
2023-02-23 07:37:09 +0000

    deploy: 6a51d08cc6e8adc53abc6284686039c5afc59b97

--- assets/js/31.js
+++ assets/js/31.js
@@ -92,17 +92,6 @@ eval("module.exports = __webpack_require__.p + \"assets/img/icon-link-new.d756c6
 
 /***/ }),
 
-/***/ "./src/modules/dashboard/img/icon-more-options.svg":
-/*!*********************************************************!*\
-  !*** ./src/modules/dashboard/img/icon-more-options.svg ***!
-  \*********************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("module.exports = __webpack_require__.p + \"assets/img/icon-more-options.54384d4a.svg\";\n\n//# sourceURL=webpack:///./src/modules/dashboard/img/icon-more-options.svg?");
-
-/***/ }),
-
 /***/ "./src/modules/services/submodule/assets/components/AssetDataTable.vue":
 /*!*****************************************************************************!*\
   !*** ./src/modules/services/submodule/assets/components/AssetDataTable.vue ***!
