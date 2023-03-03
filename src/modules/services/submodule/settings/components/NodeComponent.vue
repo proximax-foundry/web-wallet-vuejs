97959cc7deb851950e2a7b598a34c74b40730262
Kong <kongsehlen@gmail.com>
2023-03-03 12:25:03 +0800

    settings

---
+++ src/modules/services/submodule/settings/components/NodeComponent.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="border border-gray-200 px-2 py-1 h-12">
+    <div class="uppercase text-gray-400 font-light text-txs text-left mb-2">{{ $t('general.block') }}</div>
+    <input disabled="true" v-model="blockHeight" type="text" class="text_input">
+  </div>
+
+  <div class="border border-gray-200 px-2 py-1 h-12 mt-5">
+    <div class="uppercase text-gray-400 font-light text-txs text-left mb-2">{{ $t('node.currentNode') }}</div>
+    <input disabled="true" v-model="currentNode" type="text" class="text_input">
+  </div>
+
+  <div class="mt-5 border border-gray-200 px-2 py-1">
+    <div class="h-5 text-left">
+      <transition enter-active-class="animate__animated animate__fadeInUp">
+        <span v-if="showSelectTitle" class="uppercase text-gray-400 font-light text-txs text-left">{{ placeholder
+        }}</span>
+      </transition>
+    </div>
+    <div class="select selectPlugin" style="position: relative">
+      <Multiselect :placeholder="placeholder" :options="options" mode="single" :canDeselect="canDeselect"
+        v-model="selected" :maxHeight="maxHeight" @select="makeNodeSelection" @close="closeSelection" label="name">
+        <template v-slot:singlelabel="{ value }">
+          <div class="multiselect-single-label">
+            <img src="@/modules/dashboard/img/sirius-icon.svg" class="w-10 h-10 mr-2"> {{ value.name }}
+          </div>
+        </template>
+        <template v-slot:option="{ option }">
+          <img src="@/modules/dashboard/img/sirius-icon.svg" class="w-10 h-10 mr-2"> {{ option.name }}
+        </template>
+      </Multiselect>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import Multiselect from '@vueform/multiselect';
+import { computed, ref } from "vue";
+import { useToast } from "primevue/usetoast";
+import { networkState } from '@/state/networkState';
+import { NetworkStateUtils } from '@/state/utils/networkStateUtils';
+import { walletState } from '@/state/walletState';
+import { WalletUtils } from '@/util/walletUtils';
+import { useI18n } from 'vue-i18n';
+
+const toast = useToast();
+const { t } = useI18n()
+const showSelectTitle = ref(true);
+const placeholder = t('node.nodeList');
+const canDeselect = ref(false);
+const maxHeight = ref(200);
+const selected = ref('');
+
+const options = computed(() => {
+  if (!networkState.currentNetworkProfile) {
+    return []
+  }
+  let nodeList:{value:string,name:string}[] = [];
+
+  networkState.currentNetworkProfile.apiNodes.forEach((node) => {
+    nodeList.push({ value: node, name: NetworkStateUtils.buildAPIEndpointURL(node) });
+  });
+  return nodeList;
+});
+
+const currentNode = computed(() => NetworkStateUtils.buildAPIEndpointURL(networkState.selectedAPIEndpoint))
+const blockHeight = computed(() => networkState.currentNetworkProfileConfig ? networkState.currentNetworkProfileConfig.chainHeight : 0);
+
+const makeNodeSelection = (endpoint:string) => {
+  if(!walletState.currentLoggedInWallet || !networkState.currentNetworkProfile){
+    return
+  }
+  if (endpoint != networkState.selectedAPIEndpoint) {
+    showSelectTitle.value = true;
+    NetworkStateUtils.updateChainNode(endpoint);
+    console.log("Why you running ?");
+    WalletUtils.refreshAllAccountDetails(walletState.currentLoggedInWallet, networkState.currentNetworkProfile);
+    //emitter.emit('listener:reconnect');
+    toast.add({ severity: 'success', summary: t('nodes.nodes'), detail: t('node.nodeUpdated'), group: 'br-custom', life: 5000 });
+  }
+};
+
+const closeSelection = () => {
+  if (!selected.value) {
+    clearSelection();
+  }
+};
+
+const clearSelection = () => {
+  showSelectTitle.value = false;
+};
+
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/multiselect.scss";
+
+.text_input {
+  @apply w-full flex border-0 border-white drop-shadow-none filter focus: outline-none text-tsm text-gray-600 font-bold bg-white;
+}
+</style>
---
+++ src/modules/services/submodule/settings/views/ViewSettings.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="ml-2 mr-2 w-full lg:ml-auto lg:mr-auto mt-5">
+      <div class="lg:w-9/12 ml-2 mr-2 lg:ml-auto lg:mr-auto mt-5">
+        <div class="flex justify-between text-sm mb-5">
+          <div><span class="text-gray-700">Settings</span></div>
+        </div>
+        <div class="border border-gray-200 filter drop-shadow-xl py-2 sm:py-14 px-2 sm:px-28 text-center bg-white">
+            <div class="flex">
+              <div class="text-left mb-3 text-gray-700">Select Language:</div>
+              <selectLanguageModal class="text-xs mt-1.5" />
+            </div>
+            <div class="flex">
+              <div class="text-left mb-3 text-gray-700">Sound Volume:</div>
+              <selectSoundSetting />
+            </div>
+            <div class="text-left mb-3 text-gray-700">Nodes</div>
+            <NodeComponent />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+import selectLanguageModal from '@/modules/home/components/selectLanguageModal.vue';
+import NodeComponent from "@/modules/services/submodule/settings/components/NodeComponent.vue"
+import selectSoundSetting from '@/modules/services/submodule/settings/components/selectSoundSetting.vue'; 
+
+</script>
+
+<style>
+
+</style>
