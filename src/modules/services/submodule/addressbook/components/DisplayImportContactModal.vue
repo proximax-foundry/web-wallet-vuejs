295df35cd4862d66ad95b37dfdcef1c5a1bc26d0
SehKeat <97232165+SehKeat@users.noreply.github.com>
2023-03-03 12:45:43 +0800

    addressbook

---
+++ src/modules/services/submodule/addressbook/components/DisplayImportContactModal.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+    <div @click="toggleModal = !toggleModal"
+      class="cursor-pointer mt-5 py-2 px-5 rounded-md bg-blue-primary text-white text-tsm drop-shadow-lg filter hover:bg-blue-600 transition-all duration-500">
+      <img src="@/modules/services/submodule/addressbook/img/icon-download.svg"
+        class="inline-block mr-4 relative top-1">{{ $t('general.import') }}</div>
+    <transition enter-active-class="animate__animated animate__fadeInDown"
+      leave-active-class="animate__animated animate__fadeOutUp">
+      <div v-if="toggleModal" class="popup-outer fixed flex z-50">
+        <div class="modal-popup-box">
+          <div class="delete-position">
+            <font-awesome-icon icon="times" class="delete-icon-style"
+              @click="toggleModal = !toggleModal; closeModel()"></font-awesome-icon>
+          </div>
+          <div class="w-104">
+            <h1 class="default-title font-bold mt-0 mb-5">{{ $t('addressBook.importAddress') }}</h1>
+            <div class="bg-blue-200 text-left text-sm p-2 rounded-lg text-gray-800 mb-2" v-if="contactAdded > 0">{{
+              contactAdded }} {{ $t('general.contact', contactAdded) }} {{ $t('addressBook.addressBookMessage1') }}</div>
+            <div class="bg-yellow-200 text-left text-sm p-2 rounded-lg text-gray-800 mb-2" v-if="contactExisted > 0">{{
+              contactExisted }} {{ $t('general.contact', contactExisted) }} {{ $t('addressBook.addressBookMessage2') }}</div>
+            <div class="bg-red-200 text-left text-sm p-2 rounded-lg text-gray-800 mb-2"
+              v-if="contactInvalidAddress.length > 0">{{ contactInvalidAddress.length }}
+              {{ $t('general.contact', contactInvalidAddress.length) }} {{ $t('addressBook.addressBookMessage3') }}</div>
+            <div v-if="contactInvalidAddress.length > 0" class="text-left text-gray-800 mt-5">
+              <div class="font-bold mb-2">{{ $t('general.contact', contactInvalidAddress.length) }}
+                {{ $t('addressBook.addressBookMessage4') }}:</div>
+              <div v-for="contact, index in contactInvalidAddress" :key="index" class="text-left mb-2">
+                <div class="inline-block mr-3 w-40">{{ contact.label }}</div>
+                <div class="inline-block">{{ contact.address }}</div>
+              </div>
+            </div>
+            <div class="py-5 my-5 text-tsm">
+              <form enctype="multipart/form-data">
+                <input type="file" @change="onFileChange">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <div @click="toggleModal = !toggleModal; closeModel()" v-if="toggleModal"
+      class="fixed inset-0 bg-opacity-60 bg-gray-100 z-20"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { getCurrentInstance, ref } from "vue";
+import { AddressBookUtils } from '@/util/addressBookUtils';
+import { AddressBook } from '@/models/addressBook';
+import { walletState } from '@/state/walletState';
+import { useToast } from "primevue/usetoast";
+import { useI18n } from 'vue-i18n';
+
+const toggleModal = ref(false);
+const { t } = useI18n();
+const toast = useToast();
+const internalInstance = getCurrentInstance();
+const emitter = internalInstance.appContext.config.globalProperties.emitter;
+const csv = ref([]);
+const contactAdded = ref(0);
+const contactExisted = ref(0);
+const contactInvalidAddress = ref([]);
+const onFileChange = (e) => {
+  var files = e.target.files || e.dataTransfer.files;
+  if (!files.length) return;
+  createInput(files[0]);
+};
+
+const closeModel = () => {
+  contactAdded.value = 0;
+  contactExisted.value = 0;
+  contactInvalidAddress.value = [];
+};
+
+const createInput = (file) => {
+  let promise = new Promise<string>((resolve) => {
+    var reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+    reader.readAsText(file);
+  });
+
+  promise.then(
+    result => {
+      if (!walletState.currentLoggedInWallet) {
+        return;
+      }
+      const wallet = walletState.currentLoggedInWallet;
+      /* handle a successful result */
+      var array = result.match(/[^\r\n]+/g);
+      let exist = [];
+      let addContact = [];
+      let errContact = [];
+      array.shift();
+      array.forEach(element => {
+        var label, address, group, publicKey;
+        var arr = element.split(',');
+        if (arr.length > 4) {
+          // merge all array as label except the last
+          let str = '';
+          for (var a = 0; a < arr.length - 1; ++a) {
+            str = str + arr[a] + ',';
+          }
+          label = str.replace(/['"]+/g, '');
+        } else {
+          label = arr[0].replace(/['"]+/g, '');
+        }
+        address = arr[1].replace(/['"]+/g, '');
+        if (arr[2]) {
+          group = arr[2].replace(/['"]+/g, '');
+        } else {
+          group = '-none-';
+        }
+        if (arr[3]) {
+          publicKey = arr[3].replace(/['"]+/g, '');
+        } else {
+          publicKey = null;
+        }
+
+
+        // check if address or name is already in the contact book
+        // check for existing account address in wallet
+        // const accountAddIndex = wallet.accounts.findIndex((account) => account.address == address);
+        // check for existing account name in wallet
+        const accountNameIndex = wallet.accounts.findIndex((account) => account.name.toLowerCase() == label.toLowerCase());
+        const contactAddIndex = (wallet.contacts != undefined) ? wallet.contacts.findIndex((contact) => contact.address == address) : (-1);
+        const contactNameIndex = (wallet.contacts != undefined) ? wallet.contacts.findIndex((contact) => contact.name.toLowerCase() == label.toLowerCase()) : (-1);
+
+        const defaultAccount = walletState.currentLoggedInWallet.accounts.find((account) => account.default == true);
+
+        if (contactAddIndex >= 0) {
+          exist.push({ label, address, group, publicKey });
+        } else if (contactNameIndex >= 0 || accountNameIndex >= 0) {
+          const verifyContactAddress = AddressBookUtils.verifyNetworkAddress(defaultAccount.address, address);
+          if (verifyContactAddress.isPassed) {
+            addContact.push({ label: label + ' - 2', address, group, publicKey });
+          } else {
+            errContact.push({ label, address, group, publicKey });
+          }
+        } else {
+          const verifyContactAddress = AddressBookUtils.verifyNetworkAddress(defaultAccount.address, address);
+          if (verifyContactAddress.isPassed) {
+            addContact.push({ label, address, group, publicKey });
+          } else {
+            errContact.push({ label, address, group, publicKey });
+          }
+        }
+      });
+      if (exist.length > 0) {
+        // display error
+        contactExisted.value = exist.length;
+      }
+      if (addContact.length > 0) {
+        addContact.forEach((element) => {
+          let addressBook = new AddressBook(element.label, element.address, element.group, element.publicKey);
+          walletState.currentLoggedInWallet.addAddressBook(addressBook);
+        });
+        walletState.wallets.saveMyWalletOnlytoLocalStorage(walletState.currentLoggedInWallet);
+        contactAdded.value = addContact.length;
+        emitter.emit('REFRESH_CONTACT_LIST', true);
+        toast.add({ severity: 'info', summary: t('general.addressBook'), detail: t('addressBook.newContactImported', contactAdded.value), group: 'br-custom', life: 5000 });
+      }
+      contactInvalidAddress.value = errContact;
+    },
+    error => {
+      /* handle an error */
+      console.log(error);
+    }
+  );
+};
+
+</script>
+<style lang="scss" scoped>
+.import-icon:hover {
+  @apply border-blue-primary bg-blue-primary;
+
+  >div {
+    @apply text-white;
+  }
+
+  svg {
+    @apply text-white
+  }
+}</style>
---
+++ src/modules/services/submodule/addressbook/views/ViewServicesAddressBookAddContact.vue
@@ -0,0 +1,219 @@
+<template>
+  <div>
+    <div class='lg:w-9/12 ml-2 mr-2 lg:ml-auto lg:mr-auto mt-5'>
+      <div class='flex text-xs font-semibold border-b-2 menu_title_div'>
+        <router-link :to="{ name: 'ViewServicesAddressBook' }"
+          class='w-18 text-center border-b pb-3'>{{ $t('general.list') }}</router-link>
+        <router-link :to="{ name: 'ViewServicesAddressBookImport' }"
+          class='w-18 text-center border-b pb-3'>{{ $t('general.import') }}</router-link>
+        <router-link :to="{ name: 'ViewServicesAddressBookExport' }"
+          class='w-18 text-center border-b pb-3'>{{ $t('general.export') }}</router-link>
+      </div>
+      <div class="border border-gray-100 p-5 filter drop-shadow-xl mt-10 bg-white">
+        <div class="text-md mb-5 font-semibold">{{ $t('addressBook.addNewContact') }}</div>
+        <div class="error error_box mb-5" v-if="err != ''">{{ err }}</div>
+        <div class='mt-2 py-3 px-0 md:flex'>
+          <TextInputClean :placeholder="$t('general.name')" :errorMessage="$t('general.nameRequired')"
+            v-model="contactName" icon="id-card-alt" :showError="showNameErr" class="w-full md:w-52 inline-block mr-2" />
+          <TextInputClean :placeholder="$t('multisig.addressOrPk')" :errorMessage="addErr" v-model="addressOrPk"
+            icon="wallet" :showError="showAddErr" class="w-full md:w-96 inline-block mr-2" />
+          <SelectInputPluginClean v-model="selectContactGroups" :placeholder="$t('general.group')"
+            :options="contactGroups" selectDefault="-none-" ref="selectGroupDropdown"
+            class="w-full md:w-60 inline-block mr-2" />
+          <button type="submit" class="mt-5 md:mt-0 default-btn py-1 disabled:opacity-50 h-12 flex items-center"
+            :disabled="disableSave" @click="SaveContact()"><img
+              src="@/modules/services/submodule/addressbook/img/icon-save.svg" class="inline-block mr-2">
+            {{ $t('addressBook.saveAddress') }}</button>
+        </div>
+      </div>
+    </div>
+    <AddCustomGroupModal :toggleModal="isDisplayAddCustomPanel" :groups="contactGroups" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { Address } from "tsjs-xpx-chain-sdk";
+import { computed, ref, watch, getCurrentInstance } from 'vue';
+import TextInputClean from '@/components/TextInputClean.vue';
+import { useRouter } from 'vue-router';
+import { useToast } from "primevue/usetoast";
+import { AddressBook } from '@/models/addressBook';
+import { AddressBookUtils } from '@/util/addressBookUtils';
+import { walletState } from '@/state/walletState';
+import { useI18n } from 'vue-i18n'
+import SelectInputPluginClean from "@/components/SelectInputPluginClean.vue";
+import AddCustomGroupModal from "@/modules/services/submodule/addressbook/components/AddCustomGroupModal.vue";
+import { WalletUtils } from "@/util/walletUtils";
+import { AppState } from "@/state/appState";
+import { arrayExpression } from "@babel/types";
+
+const internalInstance = getCurrentInstance();
+const emitter = internalInstance.appContext.config.globalProperties.emitter;
+
+const selectGroupDropdown = ref(null);
+
+const { t } = useI18n();
+const toast = useToast();
+const contactName = ref('');
+const address = ref('');
+const addressOrPk = ref('');
+const publicKey = ref('');
+const err = ref('');
+const verifyAdd = ref(false);
+const addMsg = ref('');
+const router = useRouter();
+const selectContactGroups = ref('');
+
+const defaultGroups = ['-none-', 'Work', 'Friend', 'Family', 'Employee', 'Director'];
+let customGroup = [];
+
+const removeDuplicates = (arr) => {
+  return arr.filter((item, index) => arr.indexOf(item) === index);
+}
+
+const action = ref([]);
+action.value.push(
+  { value: '-none-', label: ' - ' },
+  { value: 'Work', label: t('addressBook.work') },
+  { value: 'Friend', label: t('addressBook.friend') },
+  { value: 'Family', label: t('addressBook.family') },
+  { value: 'Employee', label: t('addressBook.employee') },
+  { value: 'Director', label: t('addressBook.director') },
+);
+
+if (walletState.currentLoggedInWallet) {
+  if (walletState.currentLoggedInWallet.contacts.length > 0) {
+    walletState.currentLoggedInWallet.contacts.forEach(contact => {
+      if (!defaultGroups.includes(contact.group)) {
+        customGroup.push(contact.group);
+      }
+    })
+
+    let uniqueCustomGroups = removeDuplicates(customGroup);
+    if (uniqueCustomGroups.length > 0) {
+      uniqueCustomGroups.sort();
+      uniqueCustomGroups.forEach((group) => {
+        action.value.push(
+          { value: group, label: group },
+        );
+      });
+    }
+  }
+}
+
+action.value.push(
+  { value: 'Custom', label: t('addressBook.custom') },
+);
+const contactGroups = computed(() => {
+  return action.value;
+});
+
+const disableSave = computed(
+  () => !(
+    verifyAdd.value && contactName.value != ''
+  )
+);
+
+const showAddErr = computed(
+  () => !verifyAdd.value && address.value != ''
+);
+
+const showNameErr = computed(
+  () => (address.value != '' && contactName.value == '') || (addressOrPk.value != '' && contactName.value == '')
+);
+
+const addErr = computed(
+  () => {
+    let addErrDefault = t('general.invalidInput');
+    return addMsg.value ? addMsg.value : addErrDefault;
+  }
+);
+
+watch(addressOrPk, () => {
+  if (!walletState.currentLoggedInWallet) {
+    return;
+  }
+  if (addressOrPk.value.length <= 63 || addressOrPk.value.length >= 65) {
+    address.value = addressOrPk.value
+    const defaultAccount = walletState.currentLoggedInWallet.accounts.find((account) => account.default == true);
+    const verifyContactAddress = AddressBookUtils.verifyNetworkAddress(defaultAccount.address, address.value);
+    verifyAdd.value = verifyContactAddress.isPassed;
+    addMsg.value = verifyContactAddress.errMessage;
+  }
+  else if (addressOrPk.value.length == 64) {
+    verifyAdd.value = true
+    address.value = WalletUtils.createAddressFromPublicKey(addressOrPk.value, AppState.networkType).plain()
+  }
+});
+
+const isDisplayAddCustomPanel = ref(false);
+watch(selectContactGroups, (value) => {
+  if (value == 'Custom') {
+    isDisplayAddCustomPanel.value = true;
+  }
+});
+
+const getPublicKey = async (address) => {
+  try {
+    let accInfo = await AppState.chainAPI.accountAPI.getAccountInfo(Address.createFromRawAddress(address))
+    if (accInfo.publicKey == "0000000000000000000000000000000000000000000000000000000000000000") {
+      publicKey.value = null
+    }
+    else {
+      publicKey.value = accInfo.publicKey
+    }
+  }
+  catch {
+    publicKey.value = null
+  }
+}
+
+const SaveContact = async () => {
+  if (contactName.value == ' ') {
+    err.value = t('general.nameRequired');
+    return
+  }
+  if (addressOrPk.value.length == 64) {
+    publicKey.value = addressOrPk.value
+  }
+  else {
+    await getPublicKey(address.value)
+  }
+  console.log(address.value)
+  const rawAddress = Address.createFromRawAddress(address.value);
+  // let addressBook = new AddressBook(contactName.value, rawAddress.address);
+  let addressBook = new AddressBook(contactName.value.trim(), rawAddress.plain(), selectContactGroups.value, publicKey.value);
+  const wallet = walletState.currentLoggedInWallet;
+
+  // check for existing account name in wallet
+  const accountNameIndex = wallet.accounts.findIndex((account) => account.name.toLowerCase() == contactName.value.toLowerCase().trim());
+  const contactAddIndex = (wallet.contacts != undefined) ? wallet.contacts.findIndex((contact) => Address.createFromRawAddress(contact.address).plain() == rawAddress.plain()) : (-1);
+  const contactNameIndex = (wallet.contacts != undefined) ? wallet.contacts.findIndex((contact) => contact.name.toLowerCase() == contactName.value.toLowerCase().trim()) : (-1);
+
+  if (contactAddIndex >= 0) {
+    err.value = t('addressBook.addressExist');
+  } else if (contactNameIndex >= 0 || accountNameIndex >= 0) {
+    err.value = t('addressBook.nameExist');
+  } else {
+    walletState.currentLoggedInWallet.addAddressBook(addressBook);
+    walletState.wallets.saveMyWalletOnlytoLocalStorage(walletState.currentLoggedInWallet);
+    err.value = '';
+    contactName.value = '';
+    addressOrPk.value = '';
+    toast.add({ severity: 'info', summary: t('general.addressBook'), detail: t('addressBook.newContactAdded'), group: 'br-custom', life: 5000 });
+    router.push({ name: 'ViewServicesAddressBook' });
+  }
+}
+
+emitter.on('CLOSE_ADDCUSTOMGROUP_PANEL', () => {
+  isDisplayAddCustomPanel.value = false;
+});
+
+emitter.on('ADD_CUSTOM_GROUP', customGroup => {
+  action.value.push({ value: customGroup, label: customGroup });
+  if (selectGroupDropdown.value) {
+    selectGroupDropdown.value.select(customGroup);
+  }
+  isDisplayAddCustomPanel.value = false;
+});
+
+</script>
