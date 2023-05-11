ca47f3033695934f8063b610aa30e74083c44fc5
Kong Seh Len <85566370+Ghost-Slime@users.noreply.github.com>
2023-05-11 14:48:07 +0800

    Add accordion in view account (#781)
    * Add accordion in view account transaction
    
    * Add accordion in view account transaction

--- src/modules/account/views/ViewAccountFailedTransactions.vue
+++ src/modules/account/views/ViewAccountFailedTransactions.vue
@@ -12,33 +12,50 @@
                     style="background: #f3a91d">Pending</router-link>
                 <div class="border rounded-md text-white py-2 px-5" style="background: #DC143C">Failed</div>
             </div>
-            <div class="bg-white px-2 ">
-                <div class="mt-3">
-                    <FailedTxnDT :transactions="txnFailed" class="mt-3"></FailedTxnDT>
+
+        <Accordion :activeIndex="isTransactions? null:0">
+            <AccordionTab class="p-accordion-header p-highlight" :disabled="isTransactions">
+                <div class="bg-white px-2 ">
+                    <div class="mt-3">
+                        <FailedTxnDT :transactions="txnFailed" class="mt-3"></FailedTxnDT>
+                    </div>
                 </div>
-            </div>
+            </AccordionTab>
+        </Accordion>
         </div>
     </div>
 </template>
 
 <script setup lang='ts'>
 import AccountComponent from "@/modules/account/components/AccountComponent.vue";
-import { computed, ref, watch } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 import FailedTxnDT from '@/modules/dashboard/components/TransactionDataTable/FailedTxnDT.vue';
 import { AppState } from "@/state/appState";
 import AccountTabs from "@/modules/account/components/AccountTabs.vue";
 import { walletState } from "@/state/walletState";
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
 
 defineProps({
     address: String,
 })
 
 const txnStatus = ref([])
+const isTransactions = ref(true);
 const txnFailed = computed(() => {
     let txns = []
     txns = txnStatus.value
     return txns
 })
+
+let checkTransactions = async() =>{
+    if(txnFailed.value.length>0){
+        isTransactions.value = false
+    }
+    else{
+        isTransactions.value = true
+    }
+}
 
 const checkTxnStatus = async () => {
     if (!AppState.chainAPI) {
@@ -61,6 +78,7 @@ const checkTxnStatus = async () => {
 
 const init = async () => {
     await checkTxnStatus()
+    await checkTransactions()
 }
 if (AppState.isReady) {
     init();
@@ -73,4 +91,18 @@ else {
         }
     });
 }
+
+watchEffect(() => {
+      setInterval(() => {
+        init()
+      }, 1000)
+    })
+
 </script>
+<style scoped>
+:deep(.p-accordion-header,.p-highlight) {
+    background-color: white;
+    margin-top: 0px;
+    margin-bottom: 0px;
+}
+</style>
