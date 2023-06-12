08b0ca22f54c9e86eacabd0ee38747751ae6a59b
jiaming <jiaminglim0828@gmail.com>
2023-06-12 18:21:05 +0800

    remove comment

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,3 @@
-/* module.exports = {
-  plugins: [
-    tailwindcss: {},
-    autoprefixer: {},
-    require('tailwindcss'),
-    require('postcss-nested'), // or require('postcss-nesting')
-    require('postcss-custom-properties'),
-    require('autoprefixer'),
-  ]
-} */
-
 module.exports = {
   plugins: {
     tailwindcss: {},
