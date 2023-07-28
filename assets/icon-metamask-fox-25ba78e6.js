8091ace5d9769e39c4d899bc40da7262b1c56c88
anakin001 <anakin001@users.noreply.github.com>
2023-07-28 07:32:28 +0000

    deploy: b97c887fe19595658e7e49d32d820c2ea8263b77

--- assets/icon-metamask-fox-25ba78e6.js
+++
@@ -1,1 +0,0 @@
-const s="/web-wallet-vuejs/assets/icon-metamask-fox-c84729c3.svg";export{s as _};
--- assets/icon-new-page-link-270844f6.js
+++
@@ -1,1 +0,0 @@
-const s="/web-wallet-vuejs/assets/icon-warning-5cbf900b.svg",e="/web-wallet-vuejs/assets/icon-new-page-link-55588c27.svg";export{s as _,e as a};
