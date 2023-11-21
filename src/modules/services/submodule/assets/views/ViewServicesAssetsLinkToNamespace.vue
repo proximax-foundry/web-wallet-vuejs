1a1c6e2ff25110d594dc758705b2b44d6f6e3545
hong0009 <111587717+hong0009@users.noreply.github.com>
2023-11-21 16:15:44 +0800

    Fixed current supply value in Link to Namespace

--- src/modules/services/submodule/assets/views/ViewServicesAssetsLinkToNamespace.vue
+++ src/modules/services/submodule/assets/views/ViewServicesAssetsLinkToNamespace.vue
@@ -42,7 +42,7 @@
           <div class="lg:grid lg:grid-cols-2">
             <div class="my-3">
               <div class="text-xxs text-blue-primary uppercase mb-1 font-bold">{{$t('asset.currentSupply')}}<img src="@/assets/img/icon-info.svg" class="inline-block ml-2 relative" style="top: -1px;" v-tooltip.bottom="{value:'<tiptext>'+$t('asset.supplyMsg2')+'<br>'+$t('asset.supplyMsg3')+'</tiptext>', escape: true}"></div>
-              <div class="text-black font-bold text-sm">{{ assetSupply }}</div>
+              <div class="text-black font-bold text-sm">{{ assetAmount }}</div>
             </div>
             <div class="my-3">
               <div class="text-xxs text-blue-primary uppercase mb-1 font-bold">{{$t('general.divisibility')}}<img src="@/assets/img/icon-info.svg" class="inline-block ml-2 relative" style="top: -1px;" v-tooltip.bottom="{value:'<tiptext>' + $t('asset.divisibilityMsg4') + '<br><br>' + $t('asset.divisibilityMsg2') + '<br>' + $t('asset.divisibilityMsg3') + '</tiptext>', escape: true}"></div>
@@ -196,6 +196,7 @@ export default {
     const selectAsset = ref('');
     const assetDivisibility = ref(0);
     const assetSupply = ref(0);
+    const assetAmount = ref(0);
     const assetTransferable = ref(false);
     const assetMutable = ref(false);
     const selectAction = ref('link');
@@ -209,6 +210,7 @@ export default {
         assetMutable.value = asset.supplyMutable;
         assetDivisibility.value = asset.divisibility;
         assetSupply.value = Helper.convertToCurrency(asset.supply, asset.divisibility);
+        assetAmount.value = asset.amount
       }
     }
 
@@ -380,6 +382,7 @@ export default {
       transactionFee,
       transactionFeeExact,
       assetSupply,
+      assetAmount,
       assetDivisibility,
       assetTransferable,
       assetMutable,
