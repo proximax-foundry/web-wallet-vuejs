8f778a0a62dea20e6c8bcc5f9d99f794a3bab3b3
Ghost-Slime <kongsehlen@gmail.com>
2023-11-21 17:24:35 +0800

    using translation string

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,7 @@
           <router-view class="lg:ml-60 mt-10 lg:mt-16 flex-grow px-5 pt-5" v-else-if="login" :key="this.$route.path"></router-view>
           <router-view class="mt-12 sm:mt-0 flex-grow px-2 pt-5 sm:p-0" v-else></router-view>
           <footer class="md:ml-60 md:h-9 mt-10 text-center sm:text-justify sm:flex text-txs md:text-xs sm:justify-between text-gray-700 px-10 flex-grow-0" v-if="login">
-            <div class="ml-2 sm:ml-0"> Copyright © {{ lastActiveYear }}. All Rights Reserved. <a href="https://t.me/proximaxhelpdesk" target=_new class="text-blue-primary hover:underline">{{$t('home.helpdesk')}}</a> </div>
+            <div class="ml-2 sm:ml-0">{{$t('home.copyright').replace(/\b\d{4}\b/g, lastActiveYear)}} <a href="https://t.me/proximaxhelpdesk" target=_new class="text-blue-primary hover:underline">{{$t('home.helpdesk')}}</a> </div>
             <div class="mr-2 sm:mr-0 py-2 sm:py-0"><span> {{$t('home.version')}}{{ versioning }}</span></div>
           </footer>
         </div>
@@ -99,7 +99,7 @@
     </div>
     <div v-if="!login" class="w-full items-center px-2" :class="`${ overflowScreen?'relative':'2xl:absolute bottom-0' }`">
       <footer class="mx-auto h-12 mt-20 text-center  lg:flex text-txs lg:text-xs lg:justify-between container text-white pb-5">
-        <div class="ml-2 sm:ml-0"> Copyright © {{ lastActiveYear }}. All Rights Reserved. <a href="https://t.me/proximaxhelpdesk" target=_new class="text-white hover:underline">{{$t('home.helpdesk')}}</a></div>
+        <div class="ml-2 sm:ml-0">{{$t('home.copyright').replace(/\b\d{4}\b/g, lastActiveYear)}} <a href="https://t.me/proximaxhelpdesk" target=_new class="text-white hover:underline">{{$t('home.helpdesk')}}</a></div>
         <div class="mr-2 sm:mr-0 py-2 sm:py-0"><span> {{$t('home.version')}} {{ versioning }}</span></div>
       </footer>
     </div>
