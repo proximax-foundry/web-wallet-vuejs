ac5b02cfa16505c7253685c2e5b29cc6e392612a
suencheah <152125685+suencheah@users.noreply.github.com>
2023-11-29 12:39:46 +0800

    Filter tabs according to account type (#857)
    * filter tabs according to account type
    
    * try catch
    
    * add check for AppState.isReady
    
    * use AccountType.type

--- src/modules/account/components/AccountTabs.vue
+++ src/modules/account/components/AccountTabs.vue
@@ -1,17 +1,19 @@
 <template>
 <div class = 'flex text-xs flex-wrap font-semibold border-b-2 menu_title_div'>
     <router-link :class="`${selected=='details'?'border-b-2  text-blue-primary border-blue-primary':''}`" :to="{name:'ViewAccountDetails', params: { address: address}}" class= 'w-32 text-center py-3 word-break'>{{$t('account.accountDetails')}}</router-link>
-    <router-link :class="`${selected=='assets'?'border-b-2  text-blue-primary border-blue-primary':''}`" v-if="!isDelegate()" :to="{name:'ViewAccountAssets', params: { address: address}}" class= 'w-18 py-3 text-center word-break'>{{$t('general.asset',2)}}</router-link>
-    <router-link :class="`${selected=='namespaces'?'border-b-2  text-blue-primary border-blue-primary':''}`" v-if="!isDelegate()" :to="{name:'ViewAccountNamespaces', params: { address: address}}" class= 'w-24 py-3 text-center word-break'>{{$t('general.namespace',2)}}</router-link>
-    <router-link :class="`${selected=='metadata'?'border-b-2  text-blue-primary border-blue-primary':''}`" v-if="!isDelegate()" :to="{name:'ViewAccountMetadata', params: { address: address}}" class= 'w-18 text-center py-3  word-break'>Metadata</router-link>
-    <router-link :class="`${selected=='multisig'?'border-b-2  text-blue-primary border-blue-primary':''}`" v-if="!isDelegate()" :to="{name:'ViewMultisigHome', params: { address: address}}" class= 'w-18 py-3 text-center word-break'>{{$t('general.multisig')}}</router-link>
+    <router-link :class="`${selected=='assets'?'border-b-2  text-blue-primary border-blue-primary':''}`" v-if="showTabs" :to="{name:'ViewAccountAssets', params: { address: address}}" class= 'w-18 py-3 text-center word-break'>{{$t('general.asset',2)}}</router-link>
+    <router-link :class="`${selected=='namespaces'?'border-b-2  text-blue-primary border-blue-primary':''}`" v-if="showTabs" :to="{name:'ViewAccountNamespaces', params: { address: address}}" class= 'w-24 py-3 text-center word-break'>{{$t('general.namespace',2)}}</router-link>
+    <router-link :class="`${selected=='metadata'?'border-b-2  text-blue-primary border-blue-primary':''}`" v-if="showTabs" :to="{name:'ViewAccountMetadata', params: { address: address}}" class= 'w-18 text-center py-3  word-break'>Metadata</router-link>
+    <router-link :class="`${selected=='multisig'?'border-b-2  text-blue-primary border-blue-primary':''}`" v-if="showTabs" :to="{name:'ViewMultisigHome', params: { address: address}}" class= 'w-18 py-3 text-center word-break'>{{$t('general.multisig')}}</router-link>
     <router-link :class="`${selected=='txn'?'border-b-2  text-blue-primary border-blue-primary':''}`" :to="{name:'ViewAccountConfirmedTransactions', params: { address: address}}" class= 'w-18 py-3 text-center word-break'>{{$t('general.transaction',2)}}</router-link>
 </div>
-
 </template>
 
 <script lang='ts'>
+import { AppState } from '@/state/appState';
 import { walletState } from '@/state/walletState'
+import { AccountType, Address } from 'tsjs-xpx-chain-sdk';
+import { onMounted, ref } from 'vue';
 export default {
     name:"AccountTabs"
 }
@@ -23,16 +25,33 @@ const props = defineProps({
     selected: String
 })
 
-const isDelegate = ()=>{ 
-      if(!walletState.currentLoggedInWallet){
-        return false
-      }
-      let account = walletState.currentLoggedInWallet.others.find(acc=>acc.address==props.address)
-      if(account){
-        return account.type=="DELEGATE"?true:false
-      }else{
-        return false
-      }
-    }
+const showTabs = ref(false);
 
+onMounted(()=>{
+  checkAccType()
+})
+
+const checkAccType = async () => {
+  if (!AppState.isReady) {
+    setTimeout(checkAccType, 1000);
+    return
+  }
+  try {
+    let accInfo = await AppState.chainAPI!.accountAPI.getAccountInfo(Address.createFromRawAddress(props.address));
+    switch (accInfo.accountType) {
+      case AccountType.Remote:
+        showTabs.value = false;
+        break;
+      case AccountType.Remote_Unlinked:
+        showTabs.value = false;
+        break;
+      default:
+        showTabs.value = true;
+        break;
+    }
+  } catch (error) {
+    console.log('Error retrieving account type')
+    showTabs.value = false
+  }
+}
 </script>
