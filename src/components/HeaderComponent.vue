fb0213e688cd4964bca0b0fb1de3858c55c66ec2
Ghost-Slime <kongsehlen@gmail.com>
2024-02-07 13:03:16 +0800

    add missing beginner guide back

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -42,6 +42,40 @@
     </router-link>
 
     <div class=" flex items-center gap-2">
+      <div class="hidden lg:flex w-16 flex-row items-center left-gray-line relative">
+        <div class="text-center w-full h-7 cursor-pointer" @mouseover="setHoverSupportToTrue" @mouseout="setHoverSupportToFalse">
+          <img src="@/assets/img/icon-support-contact.svg" class="opacity-80 hover:opacity-100 inline-block h-4 w-4 lg:h-5 lg:w-5">
+        </div>
+        <div class="absolute z-20 w-96 text-left bg-gray-50 shadow-sm rounded-md right-0 p-2 text-tsm transition duration-200 block" style="top: 60px" v-if="isShowSupport" @mouseover="isShowSupport=true;isHoverSupportPanel=true;" @mouseout="hideSupportPanel">
+          <div class="font-bold p-2 text-txs uppercase">{{$t('home.beginnerGuide')}}</div>
+          <div class="grid grid-cols-2">
+            <div>
+              <div class="p-2">
+                <a class="mb-2 block text-blue-primary" href="https://bcdocs.xpxsirius.io/" target=_new>{{$t('home.gettingStarted')}}<img src="@/assets/img/icon-open_in_new.svg" class="inline-block -top-1 ml-1.5"></a>
+                <div class="text-txs h-10">{{$t('home.allInfo')}}</div>
+              </div>
+              <div class="p-2">
+                <a class="mb-2 block text-blue-primary" href="https://bcdocs.xpxsirius.io/docs/getting-started/what-is-proximax-sirius-chain/" target=_new>{{$t('home.siriusChain')}}<img src="@/assets/img/icon-open_in_new.svg" class="inline-block -top-1 ml-1.5"></a>
+                <div class="text-txs h-10">{{$t('home.siriusChainTitle')}}</div>
+              </div>
+            </div>
+            <div>
+              <div class="p-2">
+                <a class="mb-2 block text-blue-primary" href="https://bcdocs.xpxsirius.io/docs/built-in-features/namespace/" target=_new>{{$t('general.namespaceQues')}}<img src="@/assets/img/icon-open_in_new.svg" class="inline-block -top-1 ml-1.5"></a>
+                <div class="text-txs h-10">{{$t('home.namespaceAns')}}</div>
+              </div>
+              <div class="p-2">
+                <a class="mb-2 block text-blue-primary" href="https://bcdocs.xpxsirius.io/docs/built-in-features/mosaic/" target=_new>{{$t('general.assetQues')}}<img src="@/assets/img/icon-open_in_new.svg" class="inline-block -top-1 ml-1.5"></a>
+                <div class="text-txs h-10">{{$t('home.assetAns')}}</div>
+              </div>
+            </div>
+          </div>
+          <div class="w-full p-2 pt-3 border-t border-gray-100">
+            <a href="https://t.me/proximaxhelpdesk" target=_new class="text-xs text-blue-primary">{{$t('home.contactTeam')}}</a>
+          </div>
+        </div>
+      </div>
+
       <router-link
         :to="{ name: 'ViewNotification' }"
         class="text-center  h-7 "
@@ -115,6 +149,30 @@ const notificationLoading = ref(false)
 const notificationTimeout = ref<NodeJS.Timeout>()
 const isNewNotification = ref(false)
 const newNotificationCount = ref(0)
+
+const isHoverSupport = ref(false);
+const isShowSupport = ref(false);
+const isHoverSupportPanel = ref(false);
+const setHoverSupportToTrue = () => {
+  isHoverSupport.value = true;
+  isShowSupport.value = true;
+}
+const setHoverSupportToFalse = () => {
+  isHoverSupport.value = false;
+  setTimeout(() => {
+    if(!isHoverSupportPanel.value && !isHoverSupport.value){
+      isShowSupport.value = false;
+    }
+  }, 100);
+}
+const hideSupportPanel = () => {
+  isHoverSupportPanel.value = false;
+  setTimeout(() => {
+    if(!isHoverSupport.value && !isHoverSupportPanel.value){
+      isShowSupport.value = false;
+    }
+  }, 100);
+}
 
 const doGetNotification = async () => {
   if (!notificationLoading.value) {
