67ab0c8449669e67568d1b3b8e71f448c1dd75c4
Ghost-Slime <Ghost-Slime@users.noreply.github.com>
2024-02-28 07:52:36 +0000

    deploy: 0023bdec9b36a2b5d9cf3bbf062cc5ae898c0ce9

---
+++ assets/transactionState-3edbeb43.js
@@ -0,0 +1,1 @@
+import{aT as a}from"./index-7c20dbaf.js";const e=a({lockHashPayload:"",transactionPayload:"",unsignedTransactionPayload:"",selectedAddress:"",selectedMultisigAddress:""});export{e as T};
--- assets/transactionState-dba65b25.js
+++
@@ -1,1 +0,0 @@
-import{aT as a}from"./index-3c3de18f.js";const e=a({lockHashPayload:"",transactionPayload:"",unsignedTransactionPayload:"",selectedAddress:"",selectedMultisigAddress:""});export{e as T};
