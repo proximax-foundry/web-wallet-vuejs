3ca5e666d5286360b35a922a5a22692c2b335673
Kong <kongsehlen@gmail.com>
2024-03-07 11:36:07 +0800

    fix divisibility input in asset create

--- src/modules/services/submodule/assets/components/NumberInputClean.vue
+++ src/modules/services/submodule/assets/components/NumberInputClean.vue
@@ -2,7 +2,7 @@
   <div :class="disabled?'opacity-50':''">
     <div class="border border-gray-200 px-2 py-1 h-14 rounded-md">
       <div class="uppercase text-gray-500 text-txs text-left mb-2">{{ placeholder }} <img src="@/assets/img/icon-info.svg" class="inline-block ml-1 relative cursor-pointer" style="top: -1px;" v-tooltip.bottom="{value:'<tiptext>' + toolTip + '</tiptext>', escape: true}" v-if="toolTip"></div>
-      <input :disabled="disabled" :value="modelValue" @input="validate" @keypress="validateKey" type="number" maxlength="1" min=0 :max="max" :placeholder="placeholder" class="number_input" @click="clickInputText()" @focus="$event.target.select()">
+      <input :disabled="disabled" :value="modelValue" @input="validate" @keypress="validateKey" maxlength="1" min=0 :max="max" :placeholder="placeholder" class="number_input" @click="clickInputText()" @focus="$event.target.select()">
     </div>
     <div class="h-3 mb-2"><div class="error error-text text-left" v-if="textErr || showError">{{ errorMessage }}</div></div>
   </div>
