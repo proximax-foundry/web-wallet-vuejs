16bff8691b7f0eff30bafd6aa44f69b44cd75ad6
Ghost-Slime <kongsehlen@gmail.com>
2024-03-08 15:11:54 +0800

    fix input divisibility err

--- src/modules/services/submodule/assets/components/NumberInputClean.vue
+++ src/modules/services/submodule/assets/components/NumberInputClean.vue
@@ -2,17 +2,16 @@
   <div :class="disabled?'opacity-50':''">
     <div class="border border-gray-200 px-2 py-1 h-14 rounded-md">
       <div class="uppercase text-gray-500 text-txs text-left mb-2">{{ placeholder }} <img src="@/assets/img/icon-info.svg" class="inline-block ml-1 relative cursor-pointer" style="top: -1px;" v-tooltip.bottom="{value:'<tiptext>' + toolTip + '</tiptext>', escape: false}" v-if="toolTip"></div>
-      <input :disabled="disabled" :value="modelValue" @input="validate" @keypress="validateKey" type="number" maxlength="1" min=0 :max="max" :placeholder="placeholder" class="number_input" @click="clickInputText()" @focus="$event.target.select()">
+      <input :disabled="disabled" :value="inputValue" @input="handleInput(parseInt((<HTMLInputElement>$event.target).value))" @keypress="validateKey" type="number" min=0 :max="max" :placeholder="placeholder" class="number_input">
     </div>
     <div class="h-3 mb-2"><div class="error error-text text-left" v-if="textErr || showError">{{ errorMessage }}</div></div>
   </div>
 </template>
 
-<script>
-import Tooltip from 'primevue/tooltip';
-export default{
-  directives: { 'tooltip': Tooltip },
-  props: {
+<script setup lang="ts">
+import { getCurrentInstance, ref, watch } from 'vue';
+
+const props = defineProps({
     placeholder: String,
     errorMessage: String,
     icon: String,
@@ -22,54 +21,58 @@ export default{
     disabled: Boolean,
     max: Number,
     toolTip: String,
-  },
+})
 
-  emits:[
-    'update:modelValue'
-  ],
+const emit = defineEmits(["update:modelValue"]);
 
-  name: 'NumberInputClean',
+const internalInstance = getCurrentInstance();
+const emitter = internalInstance.appContext.config.globalProperties.emitter;
+const inputText = ref("")
+const borderColor = ref('border border-gray-300')
+const textErr = ref(false)
+const inputValue = ref(props.modelValue)
 
-  data() {
-    return {
-      inputText: "",
-      borderColor: 'border border-gray-300',
-      textErr: false,
-    };
-  },
-
-  methods: {
-    clickInputText: function() {
-      if(!this.pswdErr){
-        this.borderColor = 'border-2 border-blue-primary';
-      }
-    },
-
-    validateKey: function(e){
-      if(e.charCode < 48 || e.charCode > 54){
-        e.preventDefault();
-      }else{
-        // this.$emit('update:modelValue', e.key);
-        return e.key;
-      }
-    },
-
-    validate: function(e) {
-      // if(e.data > -1 && e.data < 7){
-        this.$emit('update:modelValue', e.data);
-      // }
-    }
-  },
-
-  mounted() {
-    this.emitter.on("CLEAR_TEXT", payload => {
-      this.inputText = payload;
-      this.textErr = false;
-      this.borderColor = 'border border-gray-300';
-    });
-
+const validateKey = (e: KeyboardEvent) => {
+  const charCode = e.key;
+  if (
+    (charCode >= '0' && charCode <= '6') ||
+    charCode === 'Backspace' ||
+    charCode === 'Delete' ||
+    charCode === 'ArrowLeft' ||
+    charCode === 'ArrowRight'
+  ) {
+    // Allow the keypress
+    return true;
+  } else {
+    // Prevent the default behavior (i.e., disallow the keypress)
+    e.preventDefault();
+    return false;
   }
 }
+
+const handleInput  = (value: number) => {
+  if(!isNaN(value)){
+    inputValue.value = value.toString();
+  }
+  else{
+    inputValue.value = "0";
+  }
+  emit('update:modelValue',  inputValue.value);
+}
+
+watch(inputValue, (newValue) => {
+  if(newValue.toString().length > 1) {
+    inputValue.value = parseInt(newValue.toString().slice(1,2)).toString()
+  }
+  emit('update:modelValue', inputValue.value);
+});
+
+emitter.on("CLEAR_TEXT", (payload: string) => {
+  inputText.value = payload;
+  textErr.value = false;
+  borderColor.value = 'border border-gray-300';
+});
+
 </script>
 <style lang="scss">
 
