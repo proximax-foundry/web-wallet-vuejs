faea8e87936369d2854ddcae2c201c57ffb01fee
Ghost-Slime <Ghost-Slime@users.noreply.github.com>
2024-11-01 08:37:19 +0000

    deploy: 9fe5751ed6e9a88af31d4e81807021ee0beb2553

--- assets/transactionState-4479ac99.js
+++
@@ -1,1 +0,0 @@
-import{a2 as a}from"./index-d2e8ccc1.js";const e=a({lockHashPayload:"",transactionPayload:"",unsignedTransactionPayload:"",selectedAddress:"",selectedMultisigAddress:""});export{e as T};
