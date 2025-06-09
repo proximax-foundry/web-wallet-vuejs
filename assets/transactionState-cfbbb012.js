e82b2878eb47be4f9d3b313bcbb8ba49c04e8281
sleepyOwl14 <sleepyOwl14@users.noreply.github.com>
2025-06-09 12:33:29 +0000

    deploy: f17bff188c96ced00c6865a356da90572117578a

--- assets/transactionState-cfbbb012.js
+++
@@ -1,1 +0,0 @@
-import{a2 as a}from"./index-5ab99b99.js";const e=a({lockHashPayload:"",transactionPayload:"",unsignedTransactionPayload:"",selectedAddress:"",selectedMultisigAddress:""});export{e as T};
