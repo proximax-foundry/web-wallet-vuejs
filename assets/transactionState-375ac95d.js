b9d1eb5404617c581ea2aaecb27739855e3a8db4
sleepyOwl14 <sleepyOwl14@users.noreply.github.com>
2025-11-19 15:17:23 +0000

    deploy: 86295cb83c00e255a14a87eeb53e7eb3d353e0f9

--- assets/transactionState-375ac95d.js
+++
@@ -1,1 +0,0 @@
-import{a2 as a}from"./index-6a78393d.js";const e=a({lockHashPayload:"",transactionPayload:"",unsignedTransactionPayload:"",selectedAddress:"",selectedMultisigAddress:""});export{e as T};
---
+++ assets/transactionState-68fa942b.js
@@ -0,0 +1,1 @@
+import{a2 as a}from"./index-94917235.js";const e=a({lockHashPayload:"",transactionPayload:"",unsignedTransactionPayload:"",selectedAddress:"",selectedMultisigAddress:""});export{e as T};
